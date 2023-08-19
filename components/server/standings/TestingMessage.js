e545494e5975cf395e372181018c27e4f9236f0f
ellis <lsp1@hotmail.com>
2023-08-19 14:56:18 -0600

    edit testing message

--- components/server/standings/TestingMessage.js
+++ components/server/standings/TestingMessage.js
@@ -1,7 +1,7 @@
 function TestingMessage() {
   return (
     <div className="w-full mt-4 mx-auto sm:rounded-lg p-3 sm:w-10/12 md:w-8/12">
-      <ol className="ms-4 mx-auto list-decimal text-justify lg:w-3/4 md:mx-auto lg:text-center">
+      <ol className="ms-4 mx-auto list-decimal text-justify lg:w-7/12 md:mx-auto ">
         <li>
           Duplicate game states have been enabled soley for testing purposes
         </li>
@@ -15,8 +15,7 @@ function TestingMessage() {
           state from the database
         </li>
         <li>
-          Game state can be edited to include the only the following team
-          abbreviations
+          Game state can be edited to include the following team abbreviations
         </li>
         <ul>
           <li>AHC, MHA, MGG, AUT, BAY, ROM, HIG, CDA</li>
