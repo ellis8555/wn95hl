9a18e074913768b4d3aa242c4d62b043e2962bca
ellis <lsp1@hotmail.com>
2023-08-19 16:26:45 -0600

    utils/api nextResponse 1rst parameter adjusted

--- utils/api/next-response.js
+++ utils/api/next-response.js
@@ -1,21 +1,14 @@
 import { NextResponse } from "next/server";
 
-function nextResponse(responseMessage, statusCode, httpMethod, cacheType) {
-  const responseConfig = {
+function nextResponse(responseMessage, statusCode, httpMethod) {
+  return NextResponse.json(responseMessage, {
     status: statusCode,
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": process.env.ALLOW_ORIGIN,
       "Access-Control-Allow-Methods": httpMethod,
     },
-  };
-
-  // Check if cacheType is provided and not null or undefined
-  if (cacheType !== undefined && cacheType !== null) {
-    responseConfig.cache = cacheType;
-  }
-
-  return NextResponse.json({ message: responseMessage }, responseConfig);
+  });
 }
 
 export default nextResponse;
