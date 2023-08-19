3f221303dd31b579014a12c3f2687e360ad55e1d
ellis <lsp1@hotmail.com>
2023-08-19 08:31:02 -0600

    testing message edit

--- components/server/standings/TestingMessage.js
+++ components/server/standings/TestingMessage.js
@@ -6,7 +6,7 @@ function TestingMessage() {
           Duplicate game states have been enabled soley for testing purposes
         </li>
         <li>Only states with the listed teams will be accepted</li>
-        <li>Clicking reset table button returns all numbers to zero</li>
+        <li>Clicking reset button returns all numbers to zero</li>
         <li>
           Game state can be edited to include the following team abbreviations
         </li>
