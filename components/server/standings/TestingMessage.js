1aa38ae2fbd7f81e8ea03394c83fc9c699cf065a
ellis <lsp1@hotmail.com>
2023-08-19 08:27:41 -0600

    test messaging widths, & make into list

--- components/server/standings/TestingMessage.js
+++ components/server/standings/TestingMessage.js
@@ -1,13 +1,19 @@
 function TestingMessage() {
   return (
-    <div className="w-full mt-4 text-center mx-auto sm:rounded-lg p-3 sm:w-3/4">
-      <ul className="flex-column justify-center">
+    <div className="w-full mt-4 mx-auto sm:rounded-lg p-3 sm:w-10/12 md:w-8/12">
+      <ol className="ms-4 mx-auto list-decimal text-justify lg:w-3/4 md:mx-auto lg:text-center">
         <li>
-          * Duplicate game states have been enabled soley for testing purposes
+          Duplicate game states have been enabled soley for testing purposes
         </li>
-        <li>** Only states with the listed teams will be accepted</li>
-        <li>*** Clicking reset table button returns all numbers to zero</li>
-      </ul>
+        <li>Only states with the listed teams will be accepted</li>
+        <li>Clicking reset table button returns all numbers to zero</li>
+        <li>
+          Game state can be edited to include the following team abbreviations
+        </li>
+        <ul>
+          <li>AHC, MHA, MGG, AUT, BAY, ROM, HIG, CDA</li>
+        </ul>
+      </ol>
     </div>
   );
 }
