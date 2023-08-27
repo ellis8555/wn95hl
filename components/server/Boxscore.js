252b0d6573838c3f7214ac34e168bd3c2ef33d79
ellis <lsp1@hotmail.com>
2023-08-27 11:50:06 -0600

    add OT information to boxscore

--- components/server/Boxscore.js
+++ components/server/Boxscore.js
@@ -10,22 +10,29 @@ function Boxscore({ boxscore }) {
     const homeScore = boxscore["homeTeam"]["homeScore"];
     const awayAcronym = boxscore["awayTeam"]["acronym"];
     const awayScore = boxscore["awayTeam"]["awayScore"];
+    const wasGameATie = boxscore["wasGameATie"];
+    const wasOvertimeRequired = boxscore["overtimeRequired"];
     return (
-      <div className="w-full flex justify-center gap-3 items-center sm:w-1/2 sm:mx-auto">
-        Home
-        <TeamLogo name={homeAcronym} width={60} height={60} />
-        <div className="text-4xl">
-          {homeScore} - {awayScore}
+      <>
+        {!wasGameATie && wasOvertimeRequired && (
+          <div className="text-center text-4xl">OT</div>
+        )}
+        <div className="w-full flex justify-center gap-3 items-center sm:w-1/2 sm:mx-auto">
+          Home
+          <TeamLogo name={homeAcronym} width={60} height={60} />
+          <div className="text-4xl">
+            {homeScore} - {awayScore}
+          </div>
+          <TeamLogo name={awayAcronym} width={60} height={60} />
+          Away
         </div>
-        <TeamLogo name={awayAcronym} width={60} height={60} />
-        Away
-      </div>
+      </>
     );
   }
 
   return (
-    <div className="w-full text-center bg-slate-200 sm:w-3/4 sm:mx-auto">
-      No score to display
+    <div className="w-full text-center bg-slate-200 mt-4 sm:w-3/4 sm:mx-auto">
+      Submit a game state
     </div>
   );
 }
