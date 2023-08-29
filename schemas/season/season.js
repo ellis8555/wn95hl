2cfb8d5a9acd560cd9d91831e299d5a9be5b60cd
ellis <lsp1@hotmail.com>
2023-08-28 20:55:06 -0600

    add fields to season schema

--- schemas/season/season.js
+++ schemas/season/season.js
@@ -1,22 +1,30 @@
 import { Schema, models, model } from "mongoose";
 import LeagueTableSchema from "../standings/league-table";
+import LeaugesTeamsSchema from "./seasonSubSchemas/leagueTeamsSchema";
 import GameResult from "../gameResults/gameResult";
 
-const SeasonScehma = new Schema({
-  teams: [String],
+const SeasonSchema = new Schema({
+  seasonNumber: String,
+  isSeasonComplete: {
+    type: Boolean,
+    default: false,
+  },
+  teams: [LeaugesTeamsSchema],
   standings: [LeagueTableSchema],
   seasonGames: [GameResult],
+  startDate: Date,
+  endDate: Date,
 });
 
 // method that will create a dynamic schema
-function getSeasonsModel(seasonNumber) {
-  const modelName = `season${seasonNumber}detail`;
+function getSeasonsModel(leagueName) {
+  const modelName = `The_${leagueName}`;
 
   if (models[modelName]) {
     return models[modelName];
   }
 
-  return model(modelName, SeasonScehma);
+  return model(modelName, SeasonSchema);
 }
 
 export default getSeasonsModel;
