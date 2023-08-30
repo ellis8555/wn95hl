4d2ef3a5f09225b20c72bae2db9444ba3a52258a
ellis <lsp1@hotmail.com>
2023-08-30 07:27:38 -0600

    create gameHighlight schema

---
+++ schemas/gameHighlights/gameHighlights.js
@@ -0,0 +1,86 @@
+import { Schema } from "mongoose";
+
+const Highlight = new Schema(
+  {
+    href: String,
+    description: {
+      type: String,
+      default: "",
+    },
+    usernameOne: {
+      type: String,
+      default: "",
+    },
+    usernameTwo: {
+      type: String,
+      default: "",
+    },
+    gameType: {
+      type: String,
+      default: "",
+    },
+    leagueName: {
+      type: String,
+      default: "",
+    },
+    seasonNumber: {
+      type: String,
+      default: "",
+    },
+    gameWinner: {
+      type: Boolean,
+      default: false,
+    },
+    seriesWinner: {
+      type: Boolean,
+      default: false,
+    },
+    championshipSeriesWinner: {
+      type: Boolean,
+      default: false,
+    },
+    goalScorer: {
+      type: String,
+      default: "",
+    },
+    goalScorerGoalNumberOfSeason: {
+      type: String,
+      default: "",
+    },
+    goalScorerGoalNumberOfPlayoffs: {
+      type: String,
+      default: "",
+    },
+    goalScorerGoalNumberOfSeasonCareer: {
+      type: String,
+      default: "",
+    },
+    goalScorerGoalNumberOfPlayoffCareer: {
+      type: String,
+      default: "",
+    },
+    goalScorerGoalNumberOfCareer: {
+      type: String,
+      default: "",
+    },
+    goalScorerGoalNumberOfCareer: {
+      type: String,
+      default: "",
+    },
+    assistOne: {
+      type: String,
+      default: "",
+    },
+    assistTwo: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const GameHighlight = mongoose.model("highlight", Highlight);
+
+export default GameHighlight;
