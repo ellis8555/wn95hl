37f4b5204d33af7a7180d70197a2615e1b5d9f8a
ellis <lsp1@hotmail.com>
2023-08-31 22:36:10 -0600

    queryifseasonexists reflects new db structure

--- utils/db-queries/query-one/season/query-if-season-exists.js
+++ utils/db-queries/query-one/season/query-if-season-exists.js
@@ -1,20 +1,16 @@
 // searches for a seasons collection to see if it exists.
 // if it does exist this returns the season's collection name
 // the returned name of a collection is used as a argument in 'queryForASeason()'
+import getSeasonsModel from "@/schemas/season/season";
 
-import mongoose from "mongoose";
+const queryForIfSeasonExists = async (db, leagueName, seasonNumber) => {
+  const leagueCollection = `${leagueName}_seasons`;
+  const SeasonModel = getSeasonsModel(leagueCollection);
 
-const queryForIfSeasonExists = async (seasonNumber) => {
-  const thisSeasonsCollection = `season${seasonNumber}details`;
-  const doesCollectionExist = await mongoose.connection.db
-    .listCollections({ name: thisSeasonsCollection })
-    .toArray();
-
-  if (doesCollectionExist.length === 0) {
-    return false;
-  }
-
-  return thisSeasonsCollection;
+  const doesSeasonExist = await SeasonModel.findOne({
+    seasonNumber: seasonNumber,
+  });
+  console.log(doesSeasonExist);
 };
 
 export default queryForIfSeasonExists;
