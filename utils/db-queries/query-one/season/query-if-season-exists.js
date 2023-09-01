d6604a9661aa87e368499928960a499558ba2bde
ellis <lsp1@hotmail.com>
2023-09-01 14:20:10 -0600

    remove paramater from queryforifseasonexists

--- utils/db-queries/query-one/season/query-if-season-exists.js
+++ utils/db-queries/query-one/season/query-if-season-exists.js
@@ -3,14 +3,13 @@
 // the returned name of a collection is used as a argument in 'queryForASeason()'
 import getSeasonsModel from "@/schemas/season/season";
 
-const queryForIfSeasonExists = async (db, leagueName, seasonNumber) => {
+const queryForIfSeasonExists = async (leagueName, seasonNumber) => {
   const leagueCollection = `${leagueName}_seasons`;
   const SeasonModel = getSeasonsModel(leagueCollection);
 
   const doesSeasonExist = await SeasonModel.findOne({
     seasonNumber: seasonNumber,
   });
-  console.log(doesSeasonExist);
 };
 
 export default queryForIfSeasonExists;
