b32e226859146db5266148bd7fdf7c7ef7077f16
ellis <lsp1@hotmail.com>
2023-09-01 15:54:07 -0600

    adjust query for a season

--- utils/db-queries/query-one/season/query-for-a-season.js
+++ utils/db-queries/query-one/season/query-for-a-season.js
@@ -1,8 +1,16 @@
 import getSeasonsModel from "@/schemas/season/season";
 
-const queryForIfSeasonExists = async (leagueName, seasonNumber) => {
-  const leagueCollection = `${leagueName}_seasons`;
-  const SeasonModel = getSeasonsModel(leagueCollection);
+const queryForIfSeasonExists = async (
+  leagueName,
+  seasonNumber,
+  createIfNotExists
+) => {
+  const leagueDocument = `${leagueName}_seasons`;
+  const SeasonModel = getSeasonsModel(leagueDocument, createIfNotExists);
+
+  if (!SeasonModel) {
+    return false; // Model doesn't exist, so the season doesn't exist
+  }
 
   const doesSeasonExist = await SeasonModel.findOne({
     seasonNumber: seasonNumber,
