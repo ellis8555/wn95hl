0edbe9ef41ff2121d06dccb33506b06b5189604a
ellis <lsp1@hotmail.com>
2023-09-05 20:03:41 -0600

    read multiple lines of game csv data

--- utils/CSV-game-state/read-game-state-file.js
+++ utils/CSV-game-state/read-game-state-file.js
@@ -18,89 +18,94 @@ async function readGameStateFile(file, seasonNumber, gameType, leagueName) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     let gameProperties = {};
+    const all_GAME_DATA_OBJECTS = [];
 
     reader.onload = (e) => {
       const fileContent = e.target.result;
       try {
         // get game stat categories
         const lines = fileContent.split("\n");
-        lines.splice(lines.length - 1, 1);
         const rows = lines[0].split("\r");
         const gameDataCategories = rows[0].split(",");
         // .csv file incorrectly has named home goals as away goals so edit required
         gameDataCategories[35] = "HomeGOALS";
 
-        // get the values of game stat categorie
-        const lines1 = fileContent.split("\n").slice(1);
-        const rows1 = lines1[0].split("\r");
-        const gameData = rows1[0].split(",");
+        // get the values of game stat categories
+        const allGameDataRows = fileContent.split("\n").slice(1);
+        allGameDataRows.splice(allGameDataRows.length - 1, 1);
 
         // combine categories and values into array of arrays
         // where each array item is an array of a key value pair of game stats
-        let gameStats = [];
-        for (let i = 0; i < gameDataCategories.length; i++) {
-          gameStats.push([gameDataCategories[i], gameData[i]]);
+        for (let i = 0; i < allGameDataRows.length; i++) {
+          let singleGameStats = [];
+          // const row = allGameDataRows[i].split("\r");
+          const gameData = allGameDataRows[i].split(",");
+          for (let i = 0; i < gameDataCategories.length; i++) {
+            singleGameStats.push([gameDataCategories[i], gameData[i]]);
+          }
+          const gameStats = singleGameStats;
+          const lengthOfGameStats = singleGameStats.length;
+
+          //////////////////////////////////
+          // begin organizing the games data
+          //////////////////////////////////
+
+          // gameStats contains array of key value pairs for all game data used in following methods
+          // lengthOfGameStats is var holding gameStats length
+
+          let GAME_DATA = {};
+
+          // get home team stats
+          GAME_DATA["homeTeamGameStats"] = extractHomeTeamData(gameStats);
+
+          // get home team player stats
+          GAME_DATA["homeTeamPlayerStats"] = extractHomePlayerStats(gameStats);
+
+          // get home team player stats
+          GAME_DATA["homeTeamGoalieStats"] = extractHomeGoalieStats(gameStats);
+
+          // get away team stats
+          GAME_DATA["awayTeamGameStats"] = extractAwayTeamData(gameStats);
+
+          // get away team player stats
+          GAME_DATA["awayTeamPlayerStats"] = extractAwayPlayerStats(gameStats);
+
+          // get away team player stats
+          GAME_DATA["awayTeamGoalieStats"] = extractAwayGoalieStats(gameStats);
+
+          // get goal scoring data for each goal scored
+          GAME_DATA["allGoalsScored"] = extractGoalData(
+            gameStats,
+            lengthOfGameStats
+          );
+
+          // get penalty data for each penalty taken
+          GAME_DATA["allPenalties"] = extractPenaltyData(
+            gameStats,
+            lengthOfGameStats
+          );
+
+          // get other game stats.
+          GAME_DATA["otherGameStats"] = extractOtherGameStats(
+            gameStats,
+            seasonNumber,
+            gameType,
+            leagueName
+          );
+
+          ////////////////////////////////////////
+          // object containing all the games data
+          ////////////////////////////////////////
+          gameProperties = {
+            currSeason: seasonNumber,
+            fileName: file.name,
+            fileSize: file.size,
+            fileType: file.type,
+            data: GAME_DATA,
+          };
+          all_GAME_DATA_OBJECTS.push(gameProperties);
         }
-        const lengthOfGameStats = gameStats.length;
-
-        //////////////////////////////////
-        // begin organizing the games data
-        //////////////////////////////////
-
-        // gameStats contains array of key value pairs for all game data used in following methods
-        // lengthOfGameStats is var holding gameStats length
-
-        let GAME_DATA = {};
-
-        // get home team stats
-        GAME_DATA["homeTeamGameStats"] = extractHomeTeamData(gameStats);
-
-        // get home team player stats
-        GAME_DATA["homeTeamPlayerStats"] = extractHomePlayerStats(gameStats);
-
-        // get home team player stats
-        GAME_DATA["homeTeamGoalieStats"] = extractHomeGoalieStats(gameStats);
-
-        // get away team stats
-        GAME_DATA["awayTeamGameStats"] = extractAwayTeamData(gameStats);
-
-        // get away team player stats
-        GAME_DATA["awayTeamPlayerStats"] = extractAwayPlayerStats(gameStats);
-
-        // get away team player stats
-        GAME_DATA["awayTeamGoalieStats"] = extractAwayGoalieStats(gameStats);
-
-        // get goal scoring data for each goal scored
-        GAME_DATA["allGoalsScored"] = extractGoalData(
-          gameStats,
-          lengthOfGameStats
-        );
-
-        // get penalty data for each penalty taken
-        GAME_DATA["allPenalties"] = extractPenaltyData(
-          gameStats,
-          lengthOfGameStats
-        );
-
-        // get other game stats.
-        GAME_DATA["otherGameStats"] = extractOtherGameStats(
-          gameStats,
-          seasonNumber,
-          gameType,
-          leagueName
-        );
-
-        ////////////////////////////////////////
-        // object containing all the games data
-        ////////////////////////////////////////
-        gameProperties = {
-          currSeason: seasonNumber,
-          fileName: file.name,
-          fileSize: file.size,
-          fileType: file.type,
-          data: GAME_DATA,
-        };
-        resolve(gameProperties);
+        resolve(all_GAME_DATA_OBJECTS[1]);
       } catch (error) {
         gameProperties = {
           data: "There was an error processing the file",
