278d5295a1802c55f70ac355879e24f865cf2d69
ellis <lsp1@hotmail.com>
2023-09-08 18:25:08 -0600

    conference tables added

--- components/client/Standings.js
+++ components/client/Standings.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { NextResponse } from "next/server";
 import Teamresults from "../server/standings/Teamresults";
 
@@ -11,6 +11,9 @@ function Standings({
   setServerMessage,
 }) {
   const [standingsArray, setStandingsArray] = useState([]);
+  const [filteredStandings, setFilteredStandings] = useState([]);
+  const [areStandingsFiltered, setAreStandingsFiltered] = useState(false);
+  const [leagueStructure, setLeagueStructure] = useState({});
   const [tableCategories, setTableCategories] = useState([
     "GP",
     "W",
@@ -20,6 +23,7 @@ function Standings({
     "Pts",
   ]);
   const [isLoading, setIsLoading] = useState(true);
+  const conferenceNameRef = useRef("league");
 
   function fetchLeagueTableData() {
     return new Promise((resolve, reject) => {
@@ -42,9 +46,11 @@ function Standings({
 
             return response.json();
           })
-          .then((getStandings) => {
-            if (getStandings.length > 0) {
-              resolve(getStandings);
+          .then((getStandingsAndLeagueStructure) => {
+            // this data is an object containing standings array
+            // and also the divisions and conferences for the league
+            if (getStandingsAndLeagueStructure.standings.length > 0) {
+              resolve(getStandingsAndLeagueStructure);
             } else {
               reject("There is no data for this season");
             }
@@ -57,8 +63,9 @@ function Standings({
 
   useEffect(() => {
     fetchLeagueTableData()
-      .then((standings) => {
-        const standingsArray = standings;
+      .then((standingsAndStructure) => {
+        setLeagueStructure(standingsAndStructure.leagueStructure);
+        const standingsArray = standingsAndStructure.standings;
         standingsArray.sort((a, b) => {
           // First, sort by 'Pts' property in descending order
           if (b.Pts - a.Pts !== 0) {
@@ -82,7 +89,6 @@ function Standings({
             }
           }
         });
-
         setStandingsArray(standingsArray);
         setIsLoading(false);
       })
@@ -92,43 +98,98 @@ function Standings({
       });
   }, [updateStandings]);
 
+  useEffect(() => {
+    if (standingsArray.length > 0) {
+      filterStandings(conferenceNameRef.current);
+    }
+  }, [standingsArray]);
+
+  function filterStandings(conference = "league") {
+    if (conference === "league") {
+      conferenceNameRef.current = leagueName;
+      setFilteredStandings(standingsArray);
+      setAreStandingsFiltered(false);
+      return;
+    }
+
+    const conferenceStandings = standingsArray.filter((eachTeam) => {
+      if (leagueStructure[eachTeam.teamAcronym].conference === conference) {
+        return eachTeam;
+      }
+    });
+
+    conferenceNameRef.current = conference;
+    setFilteredStandings(conferenceStandings);
+    setAreStandingsFiltered(true);
+  }
+
   return (
-    <table className="mb-4 w-full md:w-3/4 md:mx-auto table-auto">
-      <thead>
-        <tr>
-          <th className="text-xl">W95</th>
-          {tableCategories.map((header, index) => (
-            <th className="p-4 sm:text-xl" key={index}>
-              {header}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {isLoading ? (
+    <div className="mt-3">
+      <div className="flex flex-row justify-center gap-2">
+        <button
+          className="bg-slate-300 rounded-full p-2"
+          onClick={() => filterStandings()}
+        >
+          League
+        </button>
+        <button
+          className="bg-slate-300 rounded-full p-2"
+          onClick={() => filterStandings("Prince of Wales")}
+        >
+          Prince of Wales
+        </button>
+        <button
+          className="bg-slate-300 rounded-full p-2"
+          onClick={() => filterStandings("Clarence Campbell")}
+        >
+          Clarence Campbell
+        </button>
+      </div>
+
+      <h1 className="mt-3 text-3xl text-center">
+        The {conferenceNameRef.current}
+      </h1>
+
+      <table className="mb-4 w-full md:w-3/4 md:mx-auto table-auto">
+        <thead>
           <tr>
-            <td className="text-center" colSpan={tableCategories.length + 1}>
-              Table is Loading...
-            </td>
+            <th className="text-xl">W95</th>
+            {tableCategories.map((header, index) => (
+              <th className="p-4 sm:text-xl" key={index}>
+                {header}
+              </th>
+            ))}
           </tr>
-        ) : standingsArray.length > 0 ? (
-          standingsArray.map((team, index) => (
-            <Teamresults
-              key={index}
-              team={team}
-              categories={tableCategories}
-              bgColor={index % 2 === 0 ? "bg-slate-200" : "bg-white"}
-            />
-          ))
-        ) : (
-          <tr>
-            <td className="text-center" colSpan={tableCategories.length + 1}>
-              No games have been played
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {isLoading ? (
+            <tr>
+              <td className="text-center" colSpan={tableCategories.length + 1}>
+                Table is Loading...
+              </td>
+            </tr>
+          ) : (areStandingsFiltered ? filteredStandings : standingsArray)
+              .length > 0 ? (
+            (areStandingsFiltered ? filteredStandings : standingsArray).map(
+              (team, index) => (
+                <Teamresults
+                  key={index}
+                  team={team}
+                  categories={tableCategories}
+                  bgColor={index % 2 === 0 ? "bg-slate-200" : "bg-white"}
+                />
+              )
+            )
+          ) : (
+            <tr>
+              <td className="text-center" colSpan={tableCategories.length + 1}>
+                No games have been played
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 }
 export default Standings;
