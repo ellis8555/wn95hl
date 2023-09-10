f3565e4047aa75113755d31026a7f6d7eed1def0
ellis <lsp1@hotmail.com>
2023-09-10 09:58:53 -0600

    gameresultscore component created

---
+++ components/server/Boxscore/GameResultScore.js
@@ -0,0 +1,55 @@
+import TeamLogo from "../standings/TeamLogo";
+
+function GameResultScore({ recentGameResults }) {
+  const mostRecentGame = recentGameResults.length - 1;
+  const gameData = recentGameResults[mostRecentGame];
+
+  if (
+    gameData !== undefined &&
+    gameData !== null &&
+    Object.keys(gameData).length !== 0
+  ) {
+    const homeData = gameData.homeTeamGameStats;
+    const awayData = gameData.awayTeamGameStats;
+    const otherData = gameData.otherGameStats;
+    const boxscoreStats = {
+      overtimeRequired: otherData.overtimeRequired,
+      wasGameATie: otherData.wasGameATie,
+      homeTeam: {
+        acronym: otherData.homeTeam,
+        homeScore: homeData.HomeGOALS,
+      },
+      awayTeam: {
+        acronym: otherData.awayTeam,
+        awayScore: awayData.AwayGOALS,
+      },
+    };
+
+    const homeAcronym = boxscoreStats["homeTeam"]["acronym"];
+    const homeScore = boxscoreStats["homeTeam"]["homeScore"];
+    const awayAcronym = boxscoreStats["awayTeam"]["acronym"];
+    const awayScore = boxscoreStats["awayTeam"]["awayScore"];
+    const wasGameATie = boxscoreStats["wasGameATie"];
+    const wasOvertimeRequired = boxscoreStats["overtimeRequired"];
+    return (
+      <>
+        <div className="flex flex-col">
+          {!wasGameATie && wasOvertimeRequired && (
+            <div className="text-center text-4xl">OT</div>
+          )}
+          <div className="w-full flex justify-center gap-3 items-center sm:w-1/2 sm:mx-auto">
+            Home
+            <TeamLogo name={homeAcronym} width={60} height={60} />
+            <div className="text-4xl">
+              {homeScore} - {awayScore}
+            </div>
+            <TeamLogo name={awayAcronym} width={60} height={60} />
+            Away
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default GameResultScore;
