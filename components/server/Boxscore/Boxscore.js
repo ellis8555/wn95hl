ce0e9a3e47b2b6c572da7a44a7fcaeb4ea36896b
ellis <lsp1@hotmail.com>
2023-09-10 09:59:43 -0600

    boxscore determines how to display recent scores

--- components/server/Boxscore/Boxscore.js
+++ components/server/Boxscore/Boxscore.js
@@ -1,13 +1,21 @@
 import Ticker from "./Ticker";
+import GameResultScore from "./GameResultScore";
 
-function Boxscore({ recentGameResults }) {
+function Boxscore({ recentGameResults, isStateUploaded }) {
   return (
-    // entire score ticker container
-    <div className="hidden sm:flex flex-row justify-center my-4 gap-1">
-      {recentGameResults.map((game, index) => (
-        <Ticker gameData={game} key={index} />
-      ))}
-    </div>
+    <>
+      {/* entire score ticker container */}
+      <div className="hidden sm:flex flex-row justify-center my-4 gap-1">
+        {recentGameResults.map((game, index) => (
+          <Ticker gameData={game} key={index} />
+        ))}
+      </div>
+      {isStateUploaded && (
+        <div className="flex sm:hidden flex-row justify-center my-4 gap-1">
+          <GameResultScore recentGameResults={recentGameResults} />
+        </div>
+      )}
+    </>
   );
 }
 
