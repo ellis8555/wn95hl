b77ce9ff17db8289b8b67d0dbdd2c4b8add6e9a5
ellis <lsp1@hotmail.com>
2023-09-09 23:25:06 -0600

    enable split table

--- components/server/Buttons/TableButton.js
+++ components/server/Buttons/TableButton.js
@@ -1,10 +1,26 @@
 import React from "react";
 
-function TableButton({ setTableToBeDisplayed, children }) {
+function TableButton({
+  setTableToBeDisplayed,
+  setSecondTableToBeDisplayed,
+  setSplitTable,
+  children,
+}) {
   return (
     <button
       className="bg-slate-300 rounded-full p-2"
       onClick={() => {
+        if (children === "Conferences") {
+          setSplitTable(true);
+          setTableToBeDisplayed("Clarence Campbell");
+          setSecondTableToBeDisplayed("Prince of Wales");
+          return;
+        }
+        if (children === "League") {
+          setSplitTable(false);
+          setTableToBeDisplayed("League");
+          return;
+        }
         setTableToBeDisplayed(children);
       }}
     >
