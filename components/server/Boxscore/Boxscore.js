cd27fb65c5ca2cd6ea9fea4eaeec04375156c8c2
ellis <lsp1@hotmail.com>
2023-09-10 08:20:59 -0600

    Boxscore component takes in ticker

--- components/server/Boxscore/Boxscore.js
+++ components/server/Boxscore/Boxscore.js
@@ -1,54 +1,12 @@
-import TeamLogo from "../standings/TeamLogo";
+import Ticker from "./Ticker";
 
-function Boxscore({ gameData }) {
-  if (
-    gameData !== undefined &&
-    gameData !== null &&
-    Object.keys(gameData).length !== 0
-  ) {
-    const homeData = gameData.data.homeTeamGameStats;
-    const awayData = gameData.data.awayTeamGameStats;
-    const otherData = gameData.data.otherGameStats;
-    const boxscoreStats = {
-      overtimeRequired: otherData.overtimeRequired,
-      wasGameATie: otherData.wasGameATie,
-      homeTeam: {
-        acronym: otherData.homeTeam,
-        homeScore: homeData.HomeGOALS,
-      },
-      awayTeam: {
-        acronym: otherData.awayTeam,
-        awayScore: awayData.AwayGOALS,
-      },
-    };
-
-    const homeAcronym = boxscoreStats["homeTeam"]["acronym"];
-    const homeScore = boxscoreStats["homeTeam"]["homeScore"];
-    const awayAcronym = boxscoreStats["awayTeam"]["acronym"];
-    const awayScore = boxscoreStats["awayTeam"]["awayScore"];
-    const wasGameATie = boxscoreStats["wasGameATie"];
-    const wasOvertimeRequired = boxscoreStats["overtimeRequired"];
-    return (
-      <>
-        {!wasGameATie && wasOvertimeRequired && (
-          <div className="text-center text-4xl">OT</div>
-        )}
-        <div className="w-full flex justify-center gap-3 items-center sm:w-1/2 sm:mx-auto">
-          Home
-          <TeamLogo name={homeAcronym} width={60} height={60} />
-          <div className="text-4xl">
-            {homeScore} - {awayScore}
-          </div>
-          <TeamLogo name={awayAcronym} width={60} height={60} />
-          Away
-        </div>
-      </>
-    );
-  }
-
+function Boxscore({ recentGameResults }) {
   return (
-    <div className="w-full text-center bg-slate-200 mt-4 sm:w-3/4 sm:mx-auto">
-      Submit a game state
+    // entire score ticker container
+    <div className="hidden sm:flex flex-row justify-center my-4 gap-1">
+      {recentGameResults.map((game, index) => (
+        <Ticker gameData={game} key={index} />
+      ))}
     </div>
   );
 }
