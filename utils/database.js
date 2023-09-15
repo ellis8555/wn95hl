6940fdfac1d42ae7de38b6c5ae51f4c0896c3393
ellis <lsp1@hotmail.com>
2023-09-14 21:53:27 -0600

    db connect to dev db

--- utils/database.js
+++ utils/database.js
@@ -4,7 +4,7 @@ const connectedMessage = `Connected to wn95hl database`;
 
 export const connectToDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_DEV_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000,
