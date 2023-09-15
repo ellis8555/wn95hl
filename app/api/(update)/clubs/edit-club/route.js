4a7a9a68b39a32ebbb8c89bd49ede7338c4869e9
ellis <lsp1@hotmail.com>
2023-09-14 21:32:53 -0600

    edit club change name and returned message

--- app/api/(update)/clubs/edit-club/route.js
+++ app/api/(update)/clubs/edit-club/route.js
@@ -6,38 +6,72 @@ import nextResponse from "@/utils/api/next-response";
 let db;
 
 export const PATCH = async (req) => {
-  const { clubName, coachName, teamAcronym, teamLogo, teamBanner } =
+  const { clubName, newName, coachName, teamAcronym, teamLogo, teamBanner } =
     await req.json();
 
   try {
     db = await connectToDb();
 
     const club = await queryOneClub(clubName);
-    console.log(club);
+
     if (!club) {
       return nextResponse({ message: "Team does not exist.." }, 400, "PATCH");
     }
 
+    const listOfChanges = [];
+
     if (coachName) {
       const coach = await queryOneUser(coachName);
       club.coachId = coach._id;
+      listOfChanges.push("coach");
+    }
+
+    if (newName) {
+      club.name = newName;
+      listOfChanges.push("team name");
     }
 
     if (teamAcronym) {
       club.teamAcronym = teamAcronym;
+      listOfChanges.push("team abbreviation");
     }
 
     if (teamLogo) {
       club.teamLogo = teamLogo;
+      listOfChanges.push("team logo");
     }
 
     if (teamBanner) {
       club.teamBanner = teamBanner;
+      listOfChanges.push("team banner");
+    }
+
+    // begin to construct a string that returns message to user stating what was changed
+    const isPlural = listOfChanges.length > 1 ? true : false;
+    let returnedMessage = "";
+
+    const firstWord = listOfChanges[0];
+    const capitalize = firstWord.charAt(0).toUpperCase() + firstWord.slice(1);
+
+    listOfChanges.splice(0, 1, capitalize);
+
+    listOfChanges.forEach((field) => {
+      if (isPlural) {
+        returnedMessage += field + ", ";
+      } else {
+        returnedMessage += field + " ";
+      }
+    });
+
+    if (isPlural) {
+      returnedMessage += "have been changed";
+    } else {
+      returnedMessage += "has been changed";
     }
 
     await club.save();
 
-    return nextResponse(club, 200, "PATCH");
+    return nextResponse({ message: returnedMessage }, 200, "PATCH");
   } catch (error) {
     return nextResponse({ message: error.message }, 500, "PATCH");
   } finally {
