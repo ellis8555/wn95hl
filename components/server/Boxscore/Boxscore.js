e7464ecb83a047926de07701bd0eac9e170ed637
ellis <lsp1@hotmail.com>
2023-09-18 17:31:19 -0600

    add date to horizontal score board

--- components/server/Boxscore/Boxscore.js
+++ components/server/Boxscore/Boxscore.js
@@ -2,12 +2,39 @@ import Ticker from "./Ticker";
 import GameResultScore from "./GameResultScore";
 
 function Boxscore({ recentGameResults, isStateUploaded }) {
+  let gamesDate;
+  let gamesDay;
+  const gameDates = [0];
+
+  if (recentGameResults.length > 0) {
+    const firstGameTimestampInTicker = Date.parse(
+      recentGameResults[0].createdAt
+    );
+    gamesDate = new Date(firstGameTimestampInTicker);
+    gamesDay = gamesDate.getDate();
+    recentGameResults.forEach((game, index) => {
+      const gameTimestampTicker = Date.parse(game.createdAt);
+      const thisGamesDate = new Date(gameTimestampTicker);
+      const thisGamesDay = thisGamesDate.getDate();
+
+      if (thisGamesDay != gamesDay) {
+        gameDates.push(index);
+        gamesDay = thisGamesDay;
+      }
+    });
+  }
+
   return (
     <>
       {/* entire score ticker container */}
       <div className="hidden sm:flex flex-row justify-center my-4 gap-1">
         {recentGameResults.map((game, index) => (
-          <Ticker gameData={game} key={index} />
+          <Ticker
+            gameData={game}
+            index={index}
+            gameDateIndexes={gameDates}
+            key={index}
+          />
         ))}
       </div>
       {isStateUploaded && (
--- components/server/Boxscore/Ticker.js
+++ components/server/Boxscore/Ticker.js
@@ -1,6 +1,6 @@
 import TeamLogo from "../standings/TeamLogo";
 
-function Ticker({ gameData }) {
+function Ticker({ gameData, index, gameDateIndexes }) {
   if (
     gameData !== undefined &&
     gameData !== null &&
@@ -29,24 +29,59 @@ function Ticker({ gameData }) {
     const wasGameATie = boxscoreStats["wasGameATie"];
     const wasOvertimeRequired = boxscoreStats["overtimeRequired"];
 
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    let isNewGameDate = false;
+    let month;
+    let date;
+    if (gameDateIndexes.includes(index)) {
+      isNewGameDate = true;
+      const getGameTimestamp = Date.parse(gameData.createdAt);
+      const dateOfGame = new Date(getGameTimestamp);
+      month = months[dateOfGame.getMonth()];
+      date = dateOfGame.getDate();
+    }
+
     return (
       <>
         {/* inside ticker container */}
+        {/* set date if date is different from previous game state */}
+        {isNewGameDate && (
+          <div className="flex border border-black bg-slate-400 items-center">
+            <div className="flex flex-col items-center justify-center font-medium">
+              <div>{month}</div>
+              <div>{date}</div>
+            </div>
+          </div>
+        )}
         <div className="flex flex-row w-28 p-1 border border-black">
           {/* actual score tickers container */}
-          <div className="flex flex-col">
-            {/* away team */}
-            <div className="flex flex-row gap-4 items-center">
-              <div>
-                <TeamLogo name={awayAcronym} width={30} height={30} />
+          <div className="flex items-center">
+            <div className="flex flex-col items-center">
+              {/* away team */}
+              <div className="flex flex-row gap-4 items-center ">
+                <div>
+                  <TeamLogo name={awayAcronym} width={30} height={30} />
+                </div>
+                <div>
+                  <div>{awayScore}</div>
+                </div>
               </div>
-              <div>
-                <div>{awayScore}</div>
-              </div>
-            </div>
-            {/* home team */}
-            <div>
-              <div className="flex flex-row gap-4 items-center">
+              {/* home team */}
+              <div className="flex flex-row gap-4 items-center ">
                 <div>
                   <TeamLogo name={homeAcronym} width={30} height={30} />
                 </div>
