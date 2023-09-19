9e5afaaf3bed2c11b6950a65f13ddda2a7660372
ellis <lsp1@hotmail.com>
2023-09-19 10:17:01 -0600

    homepage logo to navbar

--- components/server/Navbar.js
+++ components/server/Navbar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { FaSignInAlt } from "react-icons/fa";
 
 function Navbar() {
@@ -7,7 +8,15 @@ function Navbar() {
       <nav className="flex flex-row gap-4 justify-between bg-slate-800 mb-2 p-2">
         <ul className="flex flex-row">
           <li>
-            <Link href="/">Home</Link>
+            <Link href="/">
+              <Image
+                src="/images/home-page/NHL95onlineBANNER2-removebg-preview-removebg-preview.png"
+                alt="NHL 95 online"
+                width={200}
+                height={200}
+                priority
+              />
+            </Link>
           </li>
         </ul>
         <ul className="flex flex-row gap-4 items-center">
