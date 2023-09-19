2ef578ebbaad3d26d6601f4518d3f21a72b9bdbe
ellis <lsp1@hotmail.com>
2023-09-19 15:03:25 -0600

    remove tailwind from root layout

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">
+      <body>
         <Navbar />
         {children}
       </body>
