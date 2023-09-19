cae218ba14ac6ba7f5e4f4a4df800efbea3bb5b8
ellis <lsp1@hotmail.com>
2023-09-19 08:37:56 -0600

    new almost blank homepage

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import GameInputForm from "@/components/client/GameInputForm";
-
 export const metadata = {
   title: "wn95hl",
   description: "Test site for wn95hl",
@@ -8,8 +6,7 @@ export const metadata = {
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl text-center">wn95hl.com affiliated test area</h1>
-      <GameInputForm leagueName="w" seasonNumber="8" />
+      <h1 className="text-3xl text-center">NHL 95</h1>
     </main>
   );
 }
