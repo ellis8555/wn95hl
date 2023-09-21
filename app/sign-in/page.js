02a1b16d77d12a3919dd9b21e64f0b7fc59fd1ab
ellis <lsp1@hotmail.com>
2023-09-21 16:00:55 -0600

    when signed in form replaced with logout

--- app/sign-in/page.js
+++ app/sign-in/page.js
@@ -1,12 +1,26 @@
+"use client";
+import { useContext } from "react";
+
+import { GetAuthorizationStatus } from "../layout";
 import SignInForm from "@/components/client/admin/SignInForm";
 
 function SignIn() {
+  const { isAuthorized } = useContext(GetAuthorizationStatus);
+
   return (
     <>
       <div className="text-2xl text-center mt-6 text-orange-400">
         Admin login
       </div>
-      <SignInForm />
+      {isAuthorized ? (
+        <div className="flex justify-center">
+          <button className="w-min p-1 mt-10 rounded-md bg-orange-500">
+            Logout
+          </button>
+        </div>
+      ) : (
+        <SignInForm />
+      )}
     </>
   );
 }
