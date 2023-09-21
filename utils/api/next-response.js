bd1c69ab4d499117c833cecd651d84f77d04b412
ellis <lsp1@hotmail.com>
2023-09-20 22:55:52 -0600

    nextResponse now accepts cookie header option

--- utils/api/next-response.js
+++ utils/api/next-response.js
@@ -1,13 +1,20 @@
 import { NextResponse } from "next/server";
 
-function nextResponse(responseMessage, statusCode, httpMethod) {
+function nextResponse(responseMessage, statusCode, httpMethod, cookie = null) {
+  const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": process.env.ALLOW_ORIGIN,
+    "Access-Control-Allow-Methods": httpMethod,
+  };
+
+  // If a cookie is provided, add it to the headers
+  if (cookie) {
+    headers["Set-Cookie"] = cookie;
+  }
+
   return NextResponse.json(responseMessage, {
     status: statusCode,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": process.env.ALLOW_ORIGIN,
-      "Access-Control-Allow-Methods": httpMethod,
-    },
+    headers,
   });
 }
 
