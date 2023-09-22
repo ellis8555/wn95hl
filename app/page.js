64ed1a8f1f836d7843a697bab0e9a356358c997f
ellis <lsp1@hotmail.com>
2023-09-22 08:05:12 -0600

    wn95hl to nhl95

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 import GameInputForm from "@/components/client/GameInputForm";
 
 export const metadata = {
-  title: "wn95hl",
-  description: "Test site for wn95hl",
+  title: "NHL95",
+  description: "Test site for NHL95",
 };
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl text-center">wn95hl.com affiliated test area</h1>
+      <h1 className="text-3xl text-center">nhl95.net affiliated test area</h1>
       <GameInputForm leagueName="w" seasonNumber="8" />
     </main>
   );
