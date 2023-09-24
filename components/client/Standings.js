dd6feca9f9a8c3d18ca2c7a6759202b6c3976e85
ellis <lsp1@hotmail.com>
2023-09-24 08:21:00 -0600

    standings component data passed as prob

--- components/client/Standings.js
+++ components/client/Standings.js
@@ -1,189 +1,71 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
+
 import Teamresults from "../server/standings/Teamresults";
+import { LEAGUE_TABLE_CATEGORIES } from "@/utils/constants/constants";
+import { COMPONENT_TABLE_BUTTON } from "@/utils/constants/component_consts";
 
-function Standings({
-  updateStandings,
-  leagueName,
-  seasonNumber,
-  setServerMessage,
-  tableToBeDisplayed,
-  secondTableToBeDisplayed,
-}) {
-  const [standingsArray, setStandingsArray] = useState([]);
-  const [filteredStandings, setFilteredStandings] = useState([]);
-  const [areStandingsFiltered, setAreStandingsFiltered] = useState(false);
-  const [leagueStructure, setLeagueStructure] = useState({});
-  const [tableCategories, setTableCategories] = useState([
-    "GP",
-    "W",
-    "L",
-    "T",
-    "OTL",
-    "Pts",
-  ]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  function fetchLeagueTableData() {
-    // var to hold custom error response from api
-    let errorMessage;
-
-    return new Promise((resolve, reject) => {
-      try {
-        const fetchTable = fetch(
-          `/api/season-data?league=${leagueName}&season-number=${seasonNumber}&field=standings`,
-          {
-            next: {
-              revalidate: 0,
-            },
-          }
-        );
-        fetchTable
-          .then(async (response) => {
-            if (!response.ok) {
-              errorMessage = await response.json();
-              reject(errorMessage);
-            } else {
-              return response.json();
-            }
-          })
-          .then((getStandingsAndLeagueStructure) => {
-            // this data is an object containing standings array
-            // and also the divisions and conferences for the league
-            if (getStandingsAndLeagueStructure.standings.length > 0) {
-              resolve(getStandingsAndLeagueStructure);
-            } else {
-              reject("There is no data for this season");
-            }
-          })
-          .catch((error) => {
-            if (errorMessage) {
-              reject(errorMessage);
-            } else {
-              reject(error);
-            }
-          });
-      } catch (error) {
-        reject(error);
-      }
-    });
-  }
-
-  useEffect(() => {
-    fetchLeagueTableData()
-      .then((standingsAndStructure) => {
-        setLeagueStructure(standingsAndStructure.leagueStructure);
-        const standingsArray = standingsAndStructure.standings;
-        standingsArray.sort((a, b) => {
-          // First, sort by 'Pts' property in descending order
-          if (b.Pts - a.Pts !== 0) {
-            return b.Pts - a.Pts;
-          } else if (b.GP - a.GP !== 0) {
-            return b.GP - a.GP;
-          } else {
-            // If 'Pts' and 'GP' are equal, check 'GP' values for zero
-            if (a.GP === 0 && b.GP === 0) {
-              // If both 'GP' values are 0, sort by 'teamName' in ascending order
-              return a.teamName.localeCompare(b.teamName);
-            } else if (a.GP === 0) {
-              // If 'GP' of 'a' is 0, it comes first
-              return -1;
-            } else if (b.GP === 0) {
-              // If 'GP' of 'b' is 0, it comes first
-              return 1;
-            } else {
-              // If 'GP' values are non-zero and equal, sort by 'teamName'
-              return a.teamName.localeCompare(b.teamName);
-            }
-          }
-        });
-        setStandingsArray(standingsArray);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        // custom api error message flashes on screen then rerender removes it
-        // address setServerMessage in the future
-        // setServerMessage(error.message);
-        setIsLoading(false);
-      });
-  }, [updateStandings]);
-
-  useEffect(() => {
-    if (standingsArray.length > 0) {
-      if (secondTableToBeDisplayed !== undefined) {
-        filterStandings(secondTableToBeDisplayed);
-        return;
-      }
-      filterStandings(tableToBeDisplayed);
-    }
-  }, [standingsArray, tableToBeDisplayed, secondTableToBeDisplayed]);
-
-  function filterStandings(conference) {
-    if (conference === "League") {
-      setAreStandingsFiltered(false);
-      return;
-    }
-
-    const conferenceStandings = standingsArray.filter((eachTeam) => {
-      if (leagueStructure[eachTeam.teamAcronym].conference === conference) {
-        return eachTeam;
-      }
-    });
-
-    setFilteredStandings(conferenceStandings);
-    setAreStandingsFiltered(true);
-  }
+function Standings({ leagueName, seasonNumber, leagueTable, leagueStructure }) {
+  const [standings, setStandings] = useState(leagueTable);
+  const [divisions] = useState(leagueStructure);
+  const [isTableFiltered, setIsTableFiltered] = useState(false);
+  const [component_table_button_args] = useState({
+    standings,
+    setStandings,
+    divisions,
+    setIsTableFiltered,
+  });
 
   return (
-    <div className="mt-3">
-      <h1 className="mt-3 text-3xl text-center">
-        {tableToBeDisplayed || secondTableToBeDisplayed}
-      </h1>
-
-      <table className="mb-4 w-full md:w-3/4 md:mx-auto table-auto">
-        <thead>
-          <tr>
-            <th className="text-xl">W95</th>
-            {tableCategories.map((header, index) => (
-              <th className="p-4 sm:text-xl" key={index}>
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {isLoading ? (
+    <>
+      <div className="flex flex-row justify-center gap-4">
+        {COMPONENT_TABLE_BUTTON("League", component_table_button_args)}
+        {COMPONENT_TABLE_BUTTON(
+          "Clarence Campbell",
+          component_table_button_args
+        )}
+        {COMPONENT_TABLE_BUTTON("Prince of Wales", component_table_button_args)}
+      </div>
+      <div className="mt-3">
+        <table className="mb-4 w-full md:w-3/4 md:mx-auto table-auto">
+          <thead>
             <tr>
-              <td className="text-center" colSpan={tableCategories.length + 1}>
-                Table is Loading...
-              </td>
+              <th className="text-xl">W95</th>
+              {LEAGUE_TABLE_CATEGORIES.map((header, index) => (
+                <th className="p-4 sm:text-xl" key={index}>
+                  {header}
+                </th>
+              ))}
             </tr>
-          ) : (areStandingsFiltered ? filteredStandings : standingsArray)
-              .length > 0 ? (
-            (areStandingsFiltered ? filteredStandings : standingsArray).map(
-              (team, index) => (
+          </thead>
+          <tbody>
+            {standings.length > 0 ? (
+              standings.map((team, index) => (
                 <Teamresults
                   key={index}
-                  areStandingsFiltered={areStandingsFiltered}
                   lineNumber={index}
                   team={team}
-                  categories={tableCategories}
+                  categories={LEAGUE_TABLE_CATEGORIES}
+                  isTableFiltered={isTableFiltered}
                   bgColor={index % 2 === 0 ? "bg-slate-200" : "bg-white"}
                 />
-              )
-            )
-          ) : (
-            <tr>
-              <td className="text-center" colSpan={tableCategories.length + 1}>
-                Season {seasonNumber} of the {leagueName.toUpperCase()} league
-                has zero games played yet
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
+              ))
+            ) : (
+              <tr>
+                <td
+                  className="text-center"
+                  colSpan={LEAGUE_TABLE_CATEGORIES.length + 1}
+                >
+                  Season {seasonNumber} of the {leagueName.toUpperCase()} league
+                  has zero games played yet
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 export default Standings;
