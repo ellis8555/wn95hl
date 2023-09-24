1a3e926cfb37eb47b86fe72b01bb643ac32f312f
ellis <lsp1@hotmail.com>
2023-09-24 08:23:36 -0600

    converted via use client

--- components/server/Buttons/TableButton.js
+++ components/server/Buttons/TableButton.js
@@ -1,27 +1,48 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 
 function TableButton({
-  setTableToBeDisplayed,
-  setSecondTableToBeDisplayed,
-  setSplitTable,
   children,
+  standings,
+  setStandings,
+  divisions,
+  setIsTableFiltered,
 }) {
+  const [leagueStandings] = useState(standings);
+  const [clarenceCampbell] = useState(
+    leagueStandings.filter((team) => {
+      if (divisions[team.teamAcronym].conference == "Clarence Campbell") {
+        return team;
+      }
+      return;
+    })
+  );
+  const [princeOfWales] = useState(
+    leagueStandings.filter((team) => {
+      if (divisions[team.teamAcronym].conference == "Prince of Wales") {
+        return team;
+      }
+      return;
+    })
+  );
+
   return (
     <button
       className="bg-slate-300 rounded p-1"
       onClick={() => {
-        if (children === "Conferences") {
-          setSplitTable(true);
-          setTableToBeDisplayed("Clarence Campbell");
-          setSecondTableToBeDisplayed("Prince of Wales");
-          return;
+        switch (children) {
+          case "Clarence Campbell":
+            setIsTableFiltered(true);
+            setStandings(clarenceCampbell);
+            break;
+          case "Prince of Wales":
+            setIsTableFiltered(true);
+            setStandings(princeOfWales);
+            break;
+          default:
+            setIsTableFiltered(false);
+            setStandings(leagueStandings);
         }
-        if (children === "League") {
-          setSplitTable(false);
-          setTableToBeDisplayed("League");
-          return;
-        }
-        setTableToBeDisplayed(children);
       }}
     >
       {children}
