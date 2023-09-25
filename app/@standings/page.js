b39798340b960d6c0d9adfc9fb89540f607a1a12
ellis <lsp1@hotmail.com>
2023-09-24 20:59:46 -0600

    standings parallel route created

---
+++ app/@standings/page.js
@@ -0,0 +1,39 @@
+import Standings from "@/components/client/Standings";
+import { API_DOMAIN, SORT_STANDINGS } from "@/utils/constants/constants";
+
+async function getStandingsAndLeagueStructure() {
+  const response = await fetch(
+    `${API_DOMAIN}/api/season-data?league=w&season-number=8&field=standings`,
+    {
+      next: {
+        revalidate: 0,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    const errorMessage = await response.json();
+    throw new Error(errorMessage.message);
+  }
+
+  const leagueAndStructure = await response.json();
+  return leagueAndStructure;
+}
+
+async function standingsPage() {
+  const leagueData = await getStandingsAndLeagueStructure();
+
+  const leagueStructure = leagueData.leagueStructure;
+  const standingsArray = leagueData.standings;
+  standingsArray.sort((a, b) => SORT_STANDINGS(a, b));
+  return (
+    <Standings
+      leagueName="w"
+      seasonNumber="8"
+      leagueTable={standingsArray}
+      leagueStructure={leagueStructure}
+    />
+  );
+}
+
+export default standingsPage;
