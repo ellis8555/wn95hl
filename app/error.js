caffcf6d366e752e39db45d5bf1f38fbe5fe1dcf
ellis <lsp1@hotmail.com>
2023-09-24 20:56:57 -0600

    craete root error.js

---
+++ app/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+function Error() {
+  return (
+    <main>
+      <h1 className="text-3xl text-center">nhl95.net affiliated test area</h1>
+      <div className="text-center mt-4">An error has occured</div>
+    </main>
+  );
+}
+
+export default Error;
