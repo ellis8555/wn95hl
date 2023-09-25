8c56cb2e7abef4a9fce842c4a86615e7079a6c64
ellis <lsp1@hotmail.com>
2023-09-24 23:02:35 -0600

    @boxscore error message edit

--- app/@boxscore/error.js
+++ app/@boxscore/error.js
@@ -1,7 +1,12 @@
 "use client";
 
 function Error({ error }) {
-  return <div className="text-center mt-4">{error.message}</div>;
+  return (
+    <>
+      <div className="text-center mt-4">Boxscores can't be loaded</div>
+      <div className="text-center mt-4">{error.message}</div>
+    </>
+  );
 }
 
 export default Error;
