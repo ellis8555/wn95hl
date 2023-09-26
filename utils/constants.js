d254ba1c2cb440b00671d48798b71af007eac6ca
ellis <lsp1@hotmail.com>
2023-09-25 21:44:27 -0600

    ORIGIN/URI added to constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,11 @@
+// origins
+export const ORIGIN = process.env.ALLOW_HOME_ORIGIN;
+// export const ORIGIN = process.env.ALLOW_NETLIFY_ORIGIN;
+// export const ORIGIN = "process.env.ALLOW_VERCEL_ORIGIN";
+
+// db connections for dev/prod
+export const URI = process.env.HOME_URI;
+// export const URI = process.env.MONGO_DEV_URI
+
+// name of cookie for authorization
 export const AUTH_COOKIE = "userAuth";
