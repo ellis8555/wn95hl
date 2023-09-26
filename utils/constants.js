5211bfea01dd34dfd34fc5c32023cc32092ef0a4
ellis <lsp1@hotmail.com>
2023-09-25 21:50:14 -0600

    modeswitch

--- utils/constants.js
+++ utils/constants.js
@@ -4,8 +4,8 @@ export const ORIGIN = process.env.ALLOW_HOME_ORIGIN;
 // export const ORIGIN = "process.env.ALLOW_VERCEL_ORIGIN";
 
 // db connections for dev/prod
-export const URI = process.env.HOME_URI;
-// export const URI = process.env.MONGO_DEV_URI
+// export const URI = process.env.HOME_URI;
+export const URI = process.env.MONGO_DEV_URI;
 
 // name of cookie for authorization
 export const AUTH_COOKIE = "userAuth";
