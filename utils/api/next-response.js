071242282b2d83b152771d7590b4b63d2ccb21a5
ellis <lsp1@hotmail.com>
2023-09-25 22:11:47 -0600

    next-response.js ORIGIN imported

--- utils/api/next-response.js
+++ utils/api/next-response.js
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
+import { ORIGIN } from "../constants/constants";
 
 function nextResponse(responseMessage, statusCode, httpMethod) {
   return NextResponse.json(responseMessage, {
     status: statusCode,
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": process.env.ALLOW_VERCEL_ORIGIN,
+      "Access-Control-Allow-Origin": ORIGIN,
       "Access-Control-Allow-Methods": httpMethod,
     },
   });
