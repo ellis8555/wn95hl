d7abb17baf9baefaa7862230eb557018f6d1f785
ellis <lsp1@hotmail.com>
2023-09-26 21:15:45 -0600

    table heights uniform in standings

--- components/server/standings/Teamresults.js
+++ components/server/standings/Teamresults.js
@@ -17,10 +17,12 @@ function Teamresults({
           : lineNumber === 15
           ? "border-slate-600 border-b-2"
           : ""
-      }`}
+      } h-10`}
     >
       <td className="flex justify-center">
-        <TeamLogo name={team.teamAcronym} width={25} height={15} />
+        <div className="flex items-center h-10">
+          <TeamLogo name={team.teamAcronym} width={25} height={15} />
+        </div>
       </td>
       {categories.map((category, index) => (
         <td key={index} category={category} className="text-center">
