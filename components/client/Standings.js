0f3352a846a46dda0dfee6c7f8d61d06c2f21707
ellis <lsp1@hotmail.com>
2023-09-28 22:38:24 -0600

    add table components

--- components/client/Standings.js
+++ components/client/Standings.js
@@ -2,8 +2,8 @@
 
 import { useState } from "react";
 
-import Teamresults from "../server/standings/Teamresults";
-import { LEAGUE_TABLE_CATEGORIES } from "@/utils/constants/constants";
+import LeagueTable from "../server/tables/LeagueTable";
+import FilteredTable from "../server/tables/FilteredTable";
 import { COMPONENT_TABLE_BUTTON } from "@/utils/constants/component_consts";
 
 function Standings({ leagueName, seasonNumber, leagueTable, leagueStructure }) {
@@ -43,42 +43,12 @@ function Standings({ leagueName, seasonNumber, leagueTable, leagueStructure }) {
       <div className="lg:hidden">
         <div className="mt-3">
           <div className="text-2xl text-center">{conference}</div>
-          <table className="mb-4 w-full md:w-3/4 md:mx-auto table-auto">
-            <thead>
-              <tr>
-                <th className="text-xl">Team</th>
-                {LEAGUE_TABLE_CATEGORIES.map((header, index) => (
-                  <th className="p-4 sm:text-xl" key={index}>
-                    {header}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {standings.length > 0 ? (
-                standings.map((team, index) => (
-                  <Teamresults
-                    key={index}
-                    lineNumber={index}
-                    team={team}
-                    categories={LEAGUE_TABLE_CATEGORIES}
-                    isTableFiltered={isTableFiltered}
-                    bgColor={index % 2 === 0 ? "bg-slate-200" : "bg-white"}
-                  />
-                ))
-              ) : (
-                <tr>
-                  <td
-                    className="text-center"
-                    colSpan={LEAGUE_TABLE_CATEGORIES.length + 1}
-                  >
-                    Season {seasonNumber} of the {leagueName.toUpperCase()}{" "}
-                    league has zero games played yet
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
+          <LeagueTable
+            leagueName={leagueName}
+            seasonNumber={seasonNumber}
+            standings={standings}
+            isTableFiltered={isTableFiltered}
+          />
         </div>
       </div>
       <div className="hidden lg:flex flex-row justify-around">
@@ -86,146 +56,36 @@ function Standings({ leagueName, seasonNumber, leagueTable, leagueStructure }) {
           <div className="flex flex-row justify-around gap-8">
             <div className="mt-3">
               <div className="text-2xl text-center">Clarence Campbell</div>
-              <table className="mb-4 w-full md:w-3/4 md:mx-auto table-auto">
-                <thead>
-                  <tr>
-                    <th className="text-xl">Team</th>
-                    {LEAGUE_TABLE_CATEGORIES.map((header, index) => (
-                      <th className="p-4 sm:text-xl" key={index}>
-                        {header}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {standings.length > 0 ? (
-                    standings
-                      .filter((team) => {
-                        if (
-                          divisions[team.teamAcronym].conference ==
-                          "Clarence Campbell"
-                        ) {
-                          return team;
-                        }
-                        return;
-                      })
-                      .map((team, index) => (
-                        <Teamresults
-                          key={index}
-                          lineNumber={index}
-                          team={team}
-                          categories={LEAGUE_TABLE_CATEGORIES}
-                          isTableFiltered={isTableFiltered}
-                          bgColor={
-                            index % 2 === 0 ? "bg-slate-200" : "bg-white"
-                          }
-                        />
-                      ))
-                  ) : (
-                    <tr>
-                      <td
-                        className="text-center"
-                        colSpan={LEAGUE_TABLE_CATEGORIES.length + 1}
-                      >
-                        Season {seasonNumber} of the {leagueName.toUpperCase()}{" "}
-                        league has zero games played yet
-                      </td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
+              <FilteredTable
+                confDivName="Clarence Campbell"
+                leagueName={leagueName}
+                seasonNumber={seasonNumber}
+                standings={standings}
+                divisions={divisions}
+                isTableFiltered={isTableFiltered}
+              />
             </div>
             <div className="mt-3">
               <div className="text-2xl text-center">Prince of Wales</div>
-              <table className="mb-4 w-full md:w-3/4 md:mx-auto table-auto">
-                <thead>
-                  <tr>
-                    <th className="text-xl">Team</th>
-                    {LEAGUE_TABLE_CATEGORIES.map((header, index) => (
-                      <th className="p-4 sm:text-xl" key={index}>
-                        {header}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {standings.length > 0 ? (
-                    standings
-                      .filter((team) => {
-                        if (
-                          divisions[team.teamAcronym].conference ==
-                          "Prince of Wales"
-                        ) {
-                          return team;
-                        }
-                        return;
-                      })
-                      .map((team, index) => (
-                        <Teamresults
-                          key={index}
-                          lineNumber={index}
-                          team={team}
-                          categories={LEAGUE_TABLE_CATEGORIES}
-                          isTableFiltered={isTableFiltered}
-                          bgColor={
-                            index % 2 === 0 ? "bg-slate-200" : "bg-white"
-                          }
-                        />
-                      ))
-                  ) : (
-                    <tr>
-                      <td
-                        className="text-center"
-                        colSpan={LEAGUE_TABLE_CATEGORIES.length + 1}
-                      >
-                        Season {seasonNumber} of the {leagueName.toUpperCase()}{" "}
-                        league has zero games played yet
-                      </td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
+              <FilteredTable
+                confDivName="Prince of Wales"
+                leagueName={leagueName}
+                seasonNumber={seasonNumber}
+                standings={standings}
+                divisions={divisions}
+                isTableFiltered={isTableFiltered}
+              />
             </div>
           </div>
         ) : (
           <div className="mt-3 w-3/4">
             <div className="text-2xl text-center">{conference}</div>
-            <table className="mb-4 w-full md:w-3/4 md:mx-auto table-auto">
-              <thead>
-                <tr>
-                  <th className="text-xl">Team</th>
-                  {LEAGUE_TABLE_CATEGORIES.map((header, index) => (
-                    <th className="p-4 sm:text-xl" key={index}>
-                      {header}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {standings.length > 0 ? (
-                  standings.map((team, index) => (
-                    <Teamresults
-                      key={index}
-                      lineNumber={index}
-                      team={team}
-                      categories={LEAGUE_TABLE_CATEGORIES}
-                      isTableFiltered={isTableFiltered}
-                      bgColor={index % 2 === 0 ? "bg-slate-200" : "bg-white"}
-                    />
-                  ))
-                ) : (
-                  <tr>
-                    <td
-                      className="text-center"
-                      colSpan={LEAGUE_TABLE_CATEGORIES.length + 1}
-                    >
-                      Season {seasonNumber} of the {leagueName.toUpperCase()}{" "}
-                      league has zero games played yet
-                    </td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
+            <LeagueTable
+              leagueName={leagueName}
+              seasonNumber={seasonNumber}
+              standings={standings}
+              isTableFiltered={isTableFiltered}
+            />
           </div>
         )}
       </div>
