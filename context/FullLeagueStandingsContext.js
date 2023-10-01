bba67e31bfd5406d72575405329a9aebb889b25b
ellis <lsp1@hotmail.com>
2023-10-01 12:13:45 -0600

    create standings context

---
+++ context/FullLeagueStandingsContext.js
@@ -0,0 +1,35 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+const FullLeagueStandingsContext = createContext();
+
+function LeagueStandingsProvider({ children }) {
+  const [clientSideStandings, setClientSideStandings] = useState({});
+  const [refreshTheStandings, setRefreshTheStandings] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    router.refresh();
+  }, [clientSideStandings]);
+
+  return (
+    <FullLeagueStandingsContext.Provider
+      value={{
+        clientSideStandings,
+        setClientSideStandings,
+        refreshTheStandings,
+        setRefreshTheStandings,
+      }}
+    >
+      {children}
+    </FullLeagueStandingsContext.Provider>
+  );
+}
+
+function useFullLeagueStandings() {
+  return useContext(FullLeagueStandingsContext);
+}
+
+export { LeagueStandingsProvider, useFullLeagueStandings };
