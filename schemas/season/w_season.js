2c567ea5a006ce5bfabdbb390ab045408c64d13e
ellis <lsp1@hotmail.com>
2023-10-03 22:00:25 -0600

    statics getMostRecentSeasonNumber() created

--- schemas/season/w_season.js
+++ schemas/season/w_season.js
@@ -5,6 +5,18 @@ const W_LeagueSchema = new Schema({
   ...SeasonSchema.obj,
 });
 
+W_LeagueSchema.statics.getMostRecentSeasonNumber = async function () {
+  const getSeasonNumberFields = await this.find({})
+    .select("seasonNumber")
+    .exec();
+  let listOfSeasonNumbers = [];
+  getSeasonNumberFields.forEach((season) => {
+    listOfSeasonNumbers.push(season.seasonNumber);
+  });
+  const mostRecentSeason = Math.max(...listOfSeasonNumbers);
+  return mostRecentSeason;
+};
+
 const W_Season = models.w_season || model("w_season", W_LeagueSchema);
 
 export default W_Season;
