4ced3630dc2aea0256966d9647222aeaf57800a3
ellis <lsp1@hotmail.com>
2023-10-03 18:58:28 -0600

    add statics to Club schema

--- schemas/club.js
+++ schemas/club.js
@@ -17,6 +17,36 @@ const ClubSchema = new Schema(
   }
 );
 
-const Club = models.Club || model("Club", ClubSchema);
+// statics
+// 1. queryIfClubExists
+// 2. queryOneClub
+// 3. queryClubDetail
+// 4. queryAllClubs
+
+ClubSchema.statics.queryIfClubExists = async function (clubName) {
+  return await this.exists({ name: clubName });
+};
+
+ClubSchema.statics.queryOneClub = async function (clubName) {
+  return await this.findOne({ name: clubName });
+};
+
+ClubSchema.statics.queryClubDetail = async function (
+  filterByProp,
+  value,
+  returnProp
+) {
+  // returns a requested club detail filtered by field that is input.
+  // example qeury args ('teamAcronym', 'AHC', 'name') returns name of club with acronym matching 'AHC'
+  // example would return 'Alberta'
+  const findClub = await this.findOne({ [filterByProp]: value });
+  return findClub[returnProp];
+};
+
+ClubSchema.statics.queryAllClubs = async function () {
+  return await this.find({});
+};
+
+const Club = models.Club || model("club", ClubSchema);
 
 export default Club;
