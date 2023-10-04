986388b025673d81672c4dfbc7d101695efae6a3
ellis <lsp1@hotmail.com>
2023-10-04 13:50:41 -0600

    add statics queryForIfSeasonExists

--- schemas/season/w_season.js
+++ schemas/season/w_season.js
@@ -5,6 +5,10 @@ const W_LeagueSchema = new Schema({
   ...SeasonSchema.obj,
 });
 
+// statics list
+// 1. getMostRecentSeasonNumber
+// 2. queryForIfSeasonExists
+
 W_LeagueSchema.statics.getMostRecentSeasonNumber = async function () {
   const getSeasonNumberFields = await this.find({})
     .select("seasonNumber")
@@ -15,6 +19,14 @@ W_LeagueSchema.statics.getMostRecentSeasonNumber = async function () {
   });
   const mostRecentSeason = Math.max(...listOfSeasonNumbers);
   return mostRecentSeason;
+};
+
+W_LeagueSchema.statics.queryForIfSeasonExists = async function (seasonNumber) {
+  const getSeason = await this.findOne({ seasonNumber: seasonNumber });
+  if (getSeason === null) {
+    return false;
+  }
+  return true;
 };
 
 const W_Season = models.w_season || model("w_season", W_LeagueSchema);
