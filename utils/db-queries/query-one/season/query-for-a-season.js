134b59f1951d28e90347240c0b17137ab9c57110
ellis <lsp1@hotmail.com>
2023-10-03 19:03:06 -0600

    edit query-for-a-season

--- utils/db-queries/query-one/season/query-for-a-season.js
+++ utils/db-queries/query-one/season/query-for-a-season.js
@@ -1,18 +1,9 @@
-import mongoose from "mongoose";
-import getSeasonsModel from "@/schemas/season/season";
+import W_Season from "@/schemas/season/w_season";
 
-const queryForIfSeasonExists = async (leagueName, seasonNumber) => {
-  const leagueCollection = `${leagueName}_season`;
-  const getLeagueModels = mongoose.modelNames();
-
-  const doesLeagueExist = getLeagueModels.includes(leagueCollection);
-
-  if (doesLeagueExist) {
-    const League = getSeasonsModel(leagueName);
-    const getSeason = await League.findOne({ seasonNumber: seasonNumber });
-    if (getSeason === null) {
-      return false;
-    }
+const queryForIfSeasonExists = async (seasonNumber) => {
+  const getSeason = await W_Season.findOne({ seasonNumber: seasonNumber });
+  if (getSeason === null) {
+    return false;
   }
   return true;
 };
