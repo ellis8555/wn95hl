a21cf186906dce9dea4232f9bada62764a5fe807
ellis <lsp1@hotmail.com>
2023-10-04 14:59:43 -0600

    connections.js vercel

--- utils/constants/connections.js
+++ utils/constants/connections.js
@@ -1,11 +1,11 @@
-export const DB_CONNECT = process.env.HOME_URI;
-// export const DB_CONNECT = process.env.MONGO_URI;
+// export const DB_CONNECT = process.env.HOME_URI;
+export const DB_CONNECT = process.env.MONGO_URI;
 // export const DB_CONNECT = process.env.MONGO_DEV_URI;
 
-export const DOMAIN = "http://localhost:3000";
-// export const DOMAIN = "https://nhl95.vercel.app";
+// export const DOMAIN = "http://localhost:3000";
+export const DOMAIN = "https://nhl95.vercel.app";
 // export const DOMAIN = "https://tiny-lokum-ab0acc.netlify.app";
 
-export const ORIGIN = process.env.ALLOW_HOME_ORIGIN;
-// export const ORIGIN = process.env.ALLOW_VERCEL_ORIGIN;
+// export const ORIGIN = process.env.ALLOW_HOME_ORIGIN;
+export const ORIGIN = process.env.ALLOW_VERCEL_ORIGIN;
 // export const ORIGIN = process.env.ALLOW_NETLIFY_ORIGIN
