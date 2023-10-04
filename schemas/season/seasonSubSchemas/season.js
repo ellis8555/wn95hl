0e21f3eef8c4c22b33ecfad45c9bd1352fc043b8
ellis <lsp1@hotmail.com>
2023-10-03 18:59:35 -0600

    turn season model into subschema

---
+++ schemas/season/seasonSubSchemas/season.js
@@ -0,0 +1,51 @@
+import { Schema } from "mongoose";
+import LeagueTableSchema from "@/schemas/standings/league-table";
+import LeaguesTeamsSchema from "./leagueTeamsSchema";
+import GameResultSchema from "@/schemas/gameResults/gameResult";
+
+const SeasonSchema = new Schema({
+  seasonNumber: String,
+  hasSeasonBegun: {
+    type: Boolean,
+    default: false,
+  },
+  hasSeasonEnded: {
+    type: Boolean,
+    default: false,
+  },
+  conferences: [String],
+  divisions: [String],
+  teams: [LeaguesTeamsSchema],
+  teamsDictCodes: {
+    type: Object,
+    default: {},
+  },
+  standings: [LeagueTableSchema],
+  seasonGames: [GameResultSchema],
+  divisionalGames: {
+    type: String,
+    default: "",
+  },
+  conferenceGames: {
+    type: String,
+    default: "",
+  },
+  otherConferenceGames: {
+    type: String,
+    default: "",
+  },
+  totalGamesToBePlayed: {
+    type: String,
+    default: "0",
+  },
+  startDate: {
+    type: Date,
+    default: null,
+  },
+  endDate: {
+    type: Date,
+    default: null,
+  },
+});
+
+export default SeasonSchema;
