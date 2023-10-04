b33db4ec7f9b315a52bc44d29b0b7ccecdc08d2a
ellis <lsp1@hotmail.com>
2023-10-03 18:58:00 -0600

    add static methods to User schema

--- schemas/user.js
+++ schemas/user.js
@@ -13,6 +13,33 @@ const UserSchema = new Schema(
   }
 );
 
-const User = models.User || model("User", UserSchema);
+// statics
+// 1. queryIfUserExists
+// 2. createNewUser
+// 3. queryOneUser
+// 4. queryAllUsers
+
+UserSchema.statics.queryIfUserExists = async function (name) {
+  const user = await this.exists({ name });
+  if (user !== null) {
+    return true;
+  }
+  return false;
+};
+
+UserSchema.statics.createNewUser = async function (name) {
+  const newUser = new User({ name });
+  return await newUser.save();
+};
+
+UserSchema.statics.queryOneUser = async function (name) {
+  return await this.findOne({ name });
+};
+
+UserSchema.statics.queryAllUsers = async function () {
+  return await this.find({});
+};
+
+const User = models.User || model("user", UserSchema);
 
 export default User;
