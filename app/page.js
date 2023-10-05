7b6cf42abab329f22ab078da99048474cbb9695d
ellis <lsp1@hotmail.com>
2023-10-05 15:53:42 -0600

    home default league and fetch recent seasonNumber

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import GameInputForm from "@/components/client/GameInputForm";
+import W_Season from "@/schemas/season/w_season";
 
 export const metadata = {
   title: "NHL95",
@@ -6,10 +7,15 @@ export const metadata = {
 };
 
 export default async function Home() {
+  const defaultLeague = "w";
+  const mostRecentWSeason = await W_Season.getMostRecentSeasonNumber();
   return (
     <>
       <h1 className="text-3xl text-center">nhl95.net affiliated test area</h1>
-      <GameInputForm leagueName="w" seasonNumber="8" />
+      <GameInputForm
+        leagueName={defaultLeague}
+        seasonNumber={mostRecentWSeason}
+      />
     </>
   );
 }
