f5ed8344c028dd1d9003fd51641e232bb6b13060
ellis <lsp1@hotmail.com>
2023-10-06 14:59:37 -0600

    switches added to constants

--- utils/constants/constants.js
+++ utils/constants/constants.js
@@ -1,3 +1,6 @@
+// when adding a new league they have to be added to
+// 1. LEAGUE_SCHEMA_SWITCH
+
 export const LEAGUE_TABLE_CATEGORIES = ["GP", "W", "L", "T", "OTL", "Pts"];
 
 export const MONTHS = [
@@ -15,6 +18,26 @@ export const MONTHS = [
   "Dec",
 ];
 export const DAYS_OF_WEEK = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+export const DEFAULT_LEAGUE = "w";
+
+export const LEAGUE_SCHEMA_SWITCH = function (leagueName, wLeagueSchema) {
+  switch (leagueName) {
+    case "w":
+      return wLeagueSchema;
+    default:
+      return wLeagueSchema;
+  }
+};
+
+export const CLEAR_LEAGUE_TABLE_SWITCH = function (leagueName, wLeagueSchema) {
+  switch (leagueName) {
+    case "w":
+      return wLeagueSchema;
+    default:
+      return wLeagueSchema;
+  }
+};
 
 export const SORT_STANDINGS = function (a, b) {
   // First, sort by 'Pts' property in descending order
