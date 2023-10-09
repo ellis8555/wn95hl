9cf7cecbf7d54102d5cc9a5e6023bea0e0af0b15
ellis <lsp1@hotmail.com>
2023-10-09 10:19:07 -0600

    promise.all standings.page

--- app/@standings/page.js
+++ app/@standings/page.js
@@ -1,25 +1,12 @@
 import Standings from "@/components/client/Standings";
 import { Suspense } from "react";
-import { DEFAULT_LEAGUE, SORT_STANDINGS } from "@/utils/constants/constants";
+import {
+  DEFAULT_LEAGUE,
+  SORT_STANDINGS,
+  MOST_RECENT_SEASON,
+} from "@/utils/constants/constants";
 import { DOMAIN } from "@/utils/constants/connections";
 
-async function getMostRecentSeason() {
-  const response = await fetch(
-    `${DOMAIN}/api/season-data?league=w&season-number=8&field=most-recent-season`,
-    {
-      next: {
-        revalidate: 0,
-      },
-    }
-  );
-  if (!response.ok) {
-    const errorMessage = await response.json();
-    throw new Error(errorMessage.message);
-  }
-
-  const responseData = await response.json();
-  return responseData;
-}
 async function getStandings() {
   const response = await fetch(
     `${DOMAIN}/api/season-data?league=w&season-number=8&field=standings`,
@@ -35,6 +22,7 @@ async function getStandings() {
   }
 
   const responseData = await response.json();
+
   return responseData;
 }
 async function getDivisionsAndConferences() {
@@ -56,15 +44,16 @@ async function getDivisionsAndConferences() {
 }
 
 async function standingsPage() {
-  const mostRecentSeason = await getMostRecentSeason();
-  const standings = await getStandings();
+  const [standings, divisionsAndConferences] = await Promise.all([
+    getStandings(),
+    getDivisionsAndConferences(),
+  ]);
   standings.sort((a, b) => SORT_STANDINGS(a, b));
-  const divisionsAndConferences = await getDivisionsAndConferences();
   return (
     <Suspense fallback={<p>Loading table...</p>}>
       <Standings
         leagueName={DEFAULT_LEAGUE}
-        seasonNumber={mostRecentSeason}
+        seasonNumber={MOST_RECENT_SEASON}
         leagueTable={standings}
         leagueStructure={divisionsAndConferences}
       />
