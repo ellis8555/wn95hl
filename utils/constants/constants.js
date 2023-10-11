86c2a4ff47e0888928a14107f9450474af238995
ellis <lsp1@hotmail.com>
2023-10-11 14:35:59 -0600

    remove sort_standings method

--- utils/constants/constants.js
+++ utils/constants/constants.js
@@ -40,31 +40,3 @@ export const CLEAR_LEAGUE_TABLE_SWITCH = function (leagueName, wLeagueSchema) {
       return;
   }
 };
-
-export const SORT_STANDINGS = function (a, b) {
-  // First, sort by 'Pts' property in descending order
-  if (b.Pts - a.Pts !== 0) {
-    return b.Pts - a.Pts;
-  } else if (b.GP - a.GP !== 0) {
-    // if teams are tied with games played then sort team with less GP placed ahead
-    return a.GP - b.GP;
-  } else if (b.GP - a.GP !== 0) {
-    // if teams pts and GP tied then sort team with more wins placed ahead
-    return b.W - a.W;
-  } else {
-    // If 'Pts' and 'GP' are equal, check 'GP' values for zero
-    if (a.GP === 0 && b.GP === 0) {
-      // If both 'GP' values are 0, sort by 'teamName' in ascending order
-      return a.teamName.localeCompare(b.teamName);
-    } else if (a.GP === 0) {
-      // If 'GP' of 'a' is 0, it comes first
-      return -1;
-    } else if (b.GP === 0) {
-      // If 'GP' of 'b' is 0, it comes first
-      return 1;
-    } else {
-      // If 'GP' values are non-zero and equal, sort by 'teamName'
-      return a.teamName.localeCompare(b.teamName);
-    }
-  }
-};
