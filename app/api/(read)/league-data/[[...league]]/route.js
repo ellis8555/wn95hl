74e8bacb64828a002fb1efe3f5dae7cb9e159b22
ellis <lsp1@hotmail.com>
2023-10-11 14:38:23 -0600

    [[...league-data]] using schema statics

--- app/api/(read)/league-data/[[...league]]/route.js
+++ app/api/(read)/league-data/[[...league]]/route.js
@@ -9,129 +9,104 @@ import {
 
 export const GET = async (req, { params }) => {
   // this will be the object that is returned
+  // list of proptertis so far contained within this object
+  // 1. standings
+  // 2. divisionsAndConferences
+  // 3. dictCodes
+  // 4. recentlyPlayedGames
   let requestedData = {};
-  // set once it's known which league is requested
-  let leagueName, seasonNumber;
 
   // check if api path requires default or specific league/sesason
   // if length is zero then return default league most recent season
   const isParams = Object.keys(params).length;
 
-  // return default league and most recent season details
+  //////////////////////////////////////////////////////////
+  // if no params have been requested return default league
+  ////////////////////////////////////////////////////////
+
   if (isParams === 0) {
-    //////////////////
-    // to be filled in
-    //////////////////
-    // grab correct league schema in order to get the correct seasons data
-    const League = LEAGUE_SCHEMA_SWITCH(DEFAULT_LEAGUE, W_Season);
-    // get season data from correct league and season number
-    const seasonData = await League.findOne({
-      seasonNumber: MOST_RECENT_SEASON,
-    });
-    /////////////////////////////////
-    // add current seasons standings
-    /////////////////////////////////
-    requestedData.standings = seasonData["standings"];
-  }
-
-  // a specific league has been requested
-  if (isParams > 0) {
-    const requestedLeagueDetails = params.league;
-    [leagueName, seasonNumber] = requestedLeagueDetails;
-
     let db;
 
     try {
       const db = await connectToDb();
-
       // grab correct league schema in order to get the correct seasons data
-      const League = LEAGUE_SCHEMA_SWITCH(leagueName, W_Season);
+      const League = LEAGUE_SCHEMA_SWITCH(DEFAULT_LEAGUE, W_Season);
 
-      // get season data from correct league and season number
-      const seasonData = await League.findOne({ seasonNumber });
+      // schema method to determine if season exists
 
-      if (!seasonData) {
+      const doesSeasonExist = await League.queryForIfSeasonExists(
+        MOST_RECENT_SEASON
+      );
+      if (!doesSeasonExist) {
         return nextResponse(
           { message: "League or season number have not been found" },
           500,
           "GET"
         );
       }
-      /////////////////////////////////
-      // add current seasons standings
-      /////////////////////////////////
-      requestedData.standings = seasonData["standings"];
 
-      //////////////////////////////////////////////////////////////////////////
-      // switch takes field key from url params responds depending on the value
-      //////////////////////////////////////////////////////////////////////////
+      // schema static to add current seasons standings
 
-      // 1. teams-conferences-and-divisions
-      // 2. team-codes
-      // 3. recent-results
-      // 4. most-recent-season
+      requestedData.standings = await League.getSortedStandings(
+        MOST_RECENT_SEASON
+      );
 
-      /////////////////////////////////////////////////////
-      // if divisions and conferences are required then add
-      /////////////////////////////////////////////////////
-      if (requestedLeagueDetails.includes("teams-conferences-and-divisions")) {
-        const leagueStructure = {};
-        seasonData["teams"].forEach((team) => {
-          leagueStructure[team.teamAcronym] = {
-            conference: team["conference"],
-            division: team["division"],
-          };
-        });
-        requestedData.divisionsAndConferences = leagueStructure;
-      }
-
-      ////////////////////////////////////////
-      // if teams dictCodes required then add
-      ////////////////////////////////////////
-      if (requestedLeagueDetails.includes("team-codes")) {
-        if (!seasonData.hasSeasonBegun) {
-          return nextResponse(
-            {
-              message: `Season ${seasonNumber} of the ${leagueName.toUpperCase()} has not officially began.`,
-            },
-            400,
-            "GET"
-          );
-        }
-        if (seasonData.hasSeasonEnded) {
-          return nextResponse(
-            {
-              message: `Season ${seasonNumber} of the ${leagueName.toUpperCase()} has officially ended.`,
-            },
-            400,
-            "GET"
-          );
-        }
-        requestedData.dictCodes = seasonData["teamsDictCodes"];
-      }
-      //////////////////////////////////
-      // if recent results are required
-      //////////////////////////////////
-      if (requestedLeagueDetails.includes("recent-results")) {
-        const howManyGamesPlayed = seasonData["seasonGames"].length;
-
-        let recentlyPlayedGames = [];
-        if (howManyGamesPlayed < 8) {
-          seasonData["seasonGames"].forEach((game) =>
-            recentlyPlayedGames.push(game)
-          );
-        } else {
-          recentlyPlayedGames = seasonData["seasonGames"].slice(
-            howManyGamesPlayed - 8
-          );
-        }
-
-        requestedData.recentlyPlayedGames = recentlyPlayedGames;
-      }
-
-      ////////////////////////////
       // return requested data
-      ////////////////////////////
+
+      return nextResponse(requestedData, 200, "GET");
+    } catch (error) {
+      return nextResponse(error.message, 500, "GET");
+    } finally {
+      if (db) {
+        db.close();
+      }
+    }
+  }
+
+  ////////////////////////////////////////
+  // a specific league has been requested
+  ////////////////////////////////////////
+
+  if (isParams > 0) {
+    const requestedLeagueDetails = params.league;
+    const [leagueName, seasonNumber] = requestedLeagueDetails;
+
+    let db;
+
+    try {
+      const db = await connectToDb();
+      // grab correct league schema in order to get the correct seasons data
+      const League = LEAGUE_SCHEMA_SWITCH(leagueName, W_Season);
+
+      // schema method to determine if season exists
+
+      const doesSeasonExist = await League.queryForIfSeasonExists(seasonNumber);
+      if (!doesSeasonExist) {
+        return nextResponse(
+          { message: "League or season number have not been found" },
+          500,
+          "GET"
+        );
+      }
+
+      // schema static to add current seasons standings
+
+      requestedData.standings = await League.getSortedStandings(seasonNumber);
+
+      //////////////////////////////////////////////////////////////////
+      // schema method takes in parameter list and requestedData object
+      // requestedData object has properties added within this method
+      //////////////////////////////////////////////////////////////////
+
+      if (requestedLeagueDetails.length > 2) {
+        await League.getFieldData(
+          seasonNumber,
+          requestedLeagueDetails,
+          requestedData
+        );
+      }
+
+      // return requested data
 
       return nextResponse(requestedData, 200, "GET");
     } catch (error) {
