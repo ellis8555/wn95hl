9f3b860b3bee00446a41ed3cf5b9cd8f24c99aed
ellis <lsp1@hotmail.com>
2023-10-21 22:21:59 -0600

    create constant methods for league data retrieval

--- utils/constants/api_consts.js
+++ utils/constants/api_consts.js
@@ -1,22 +1,52 @@
-export const API_READ_SEASON_DATA = async function (
-  domain,
-  league,
+import { connectToDb } from "../database";
+import W_Season from "@/schemas/season/w_season";
+
+export const LEAGUE_SCHEMA_SWITCH = (leagueName) => {
+  switch (leagueName) {
+    case "w":
+      return W_Season;
+    default:
+      return W_Season;
+  }
+};
+
+// returns an object
+export const READ_SEASON_FIELD_DATA = async (
+  leagueName,
   seasonNumber,
-  field
-) {
-  const response = await fetch(
-    `${domain}/api/season-data?league=${league}&season-number=${seasonNumber}&field=${field}`,
-    {
-      next: {
-        revalidate: 0,
-      },
-    }
-  );
-  if (!response.ok) {
-    const errorMessage = await response.json();
-    throw new Error(errorMessage.message);
+  requestedField
+) => {
+  await connectToDb();
+
+  const League = await getLeagueAndSeason(leagueName, seasonNumber);
+
+  const fieldData = {};
+  await League.getFieldData(seasonNumber, requestedField, fieldData);
+
+  return fieldData;
+};
+
+export const READ_SEASON_STANDINGS = async (leagueName, seasonNumber) => {
+  await connectToDb();
+
+  const League = await getLeagueAndSeason(leagueName, seasonNumber);
+
+  const standings = await League.getSortedStandings(seasonNumber);
+
+  return standings;
+};
+
+////////////////////////////////////////////
+// helper methods for above constant methods
+////////////////////////////////////////////
+
+const getLeagueAndSeason = async (leagueName, seasonNumber) => {
+  const League = LEAGUE_SCHEMA_SWITCH(leagueName);
+
+  const doesSeasonExist = await League.queryForIfSeasonExists(seasonNumber);
+  if (!doesSeasonExist) {
+    throw new Error(`Season ${seasonNumber} does not exist`);
   }
 
-  const responseData = await response.json();
-  return responseData;
+  return League;
 };
--- utils/constants/constants.js
+++ utils/constants/constants.js
@@ -1,6 +1,3 @@
-// when adding a new league they have to be added to
-// 1. LEAGUE_SCHEMA_SWITCH
-
 export const LEAGUE_TABLE_CATEGORIES = ["GP", "W", "L", "T", "OTL", "Pts"];
 
 export const MONTHS = [
@@ -22,15 +19,6 @@ export const DAYS_OF_WEEK = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 export const DEFAULT_LEAGUE = "w";
 
 export const MOST_RECENT_SEASON = "8";
-
-export const LEAGUE_SCHEMA_SWITCH = function (leagueName, wLeagueSchema) {
-  switch (leagueName) {
-    case "w":
-      return wLeagueSchema;
-    default:
-      return wLeagueSchema;
-  }
-};
 
 export const CLEAR_LEAGUE_TABLE_SWITCH = function (leagueName, wLeagueSchema) {
   switch (leagueName) {
