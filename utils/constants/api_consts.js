4eede1f703d23eadd5b03056b98ac4f3d8716079
ellis <lsp1@hotmail.com>
2023-10-22 21:40:16 -0600

    api_consts change arrow to declaration functions

--- utils/constants/api_consts.js
+++ utils/constants/api_consts.js
@@ -1,50 +1,50 @@
 import W_Season from "@/schemas/season/w_season";
 
-export const LEAGUE_SCHEMA_SWITCH = async (leagueName) => {
+export async function LEAGUE_SCHEMA_SWITCH(leagueName) {
   switch (leagueName) {
     case "w":
       return W_Season;
     default:
       return W_Season;
   }
-};
+}
 
 // returns an object
-export const READ_SEASON_FIELD_DATA = async (
+export async function READ_SEASON_FIELD_DATA(
   leagueName,
   seasonNumber,
   requestedField
-) => {
+) {
   const League = await getLeagueAndSeason(leagueName, seasonNumber);
 
   const fieldData = {};
   await League.getFieldData(seasonNumber, requestedField, fieldData);
 
   return fieldData;
-};
+}
 
-export const READ_SEASON_STANDINGS = async (leagueName, seasonNumber) => {
+export async function READ_SEASON_STANDINGS(leagueName, seasonNumber) {
   const League = await getLeagueAndSeason(leagueName, seasonNumber);
 
   const standings = await League.getSortedStandings(seasonNumber);
 
   return standings;
-};
+}
 
-export const CLEAR_LEAGUE_TABLE_SWITCH = (leagueName) => {
+export async function CLEAR_LEAGUE_TABLE_SWITCH(leagueName) {
   switch (leagueName) {
     case "w":
       return W_Season;
     default:
       return;
   }
-};
+}
 
 ////////////////////////////////////////////
 // helper methods for above constant methods
 ////////////////////////////////////////////
 
-const getLeagueAndSeason = async (leagueName, seasonNumber) => {
+async function getLeagueAndSeason(leagueName, seasonNumber) {
   const leagueSchema = await LEAGUE_SCHEMA_SWITCH(leagueName);
 
   const doesSeasonExist = await leagueSchema.queryForIfSeasonExists(
@@ -55,4 +55,4 @@ const getLeagueAndSeason = async (leagueName, seasonNumber) => {
   }
 
   return leagueSchema;
-};
+}
