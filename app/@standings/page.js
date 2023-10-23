56df7f806794f0f6786b21b59bf51bf414826d0a
ellis <lsp1@hotmail.com>
2023-10-22 22:15:16 -0600

    standings par route break methods down

--- app/@standings/page.js
+++ app/@standings/page.js
@@ -12,30 +12,31 @@ import { connectToDb } from "@/utils/database";
 export const dynamic = "force-dynamic";
 
 async function getStandings(leagueName, seasonNumber) {
-  await connectToDb();
+  const standings = await READ_SEASON_STANDINGS(leagueName, seasonNumber);
 
-  const responseData = {};
+  return JSON.stringify(standings);
+}
 
-  responseData.standings = await READ_SEASON_STANDINGS(
-    leagueName,
-    seasonNumber
-  );
-
+async function getDivisionsAndConferences(leagueName, seasonNumber) {
   const { divisionsAndConferences } = await READ_SEASON_FIELD_DATA(
     leagueName,
     seasonNumber,
     "teams-conferences-and-divisions"
   );
-
-  responseData.divisionsAndConferences = divisionsAndConferences;
-  return JSON.stringify(responseData);
+  return JSON.stringify(divisionsAndConferences);
 }
 
 async function standingsPage() {
-  const { standings, divisionsAndConferences } = JSON.parse(
+  await connectToDb();
+  const standings = JSON.parse(
     await getStandings(DEFAULT_LEAGUE, MOST_RECENT_SEASON)
   );
+  const divisionsAndConferences = JSON.parse(
+    await getDivisionsAndConferences(DEFAULT_LEAGUE, MOST_RECENT_SEASON)
+  );
 
+  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+  console.log(divisionsAndConferences);
   return (
     <Standings
       leagueName={DEFAULT_LEAGUE}
