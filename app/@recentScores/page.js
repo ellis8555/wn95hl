091b480f9f7cae0d397fb8023bc3834052fbb59f
ellis <lsp1@hotmail.com>
2023-10-23 10:22:04 -0600

    revalidate parallel routes to zero

--- app/@recentScores/page.js
+++ app/@recentScores/page.js
@@ -7,6 +7,8 @@ import {
 import { READ_SEASON_FIELD_DATA } from "@/utils/constants/api_consts";
 import { connectToDb } from "@/utils/database";
 
+export const revalidate = 0;
+
 async function getRecentGameResults(leagueName, seasonNumber) {
   await connectToDb();
 
