1ccdefd2b6281af94e23953a61a856e5f67a5424
ellis <lsp1@hotmail.com>
2023-10-23 19:26:05 -0600

    remove then import api methods

--- app/@recentScores/page.js
+++ app/@recentScores/page.js
@@ -1,24 +1,10 @@
 import Boxscore from "@/components/server/Boxscore/Boxscore";
 import GameResultScore from "@/components/server/Boxscore/GameResultScore";
+import { getRecentGameResults } from "./(helpers)/get-recent-game-results";
 import {
   DEFAULT_LEAGUE,
   MOST_RECENT_SEASON,
 } from "@/utils/constants/constants";
-import { READ_SEASON_FIELD_DATA } from "@/utils/constants/api_consts";
-import { connectToDb } from "@/utils/database";
-
-export const revalidate = 1;
-
-async function getRecentGameResults(leagueName, seasonNumber) {
-  await connectToDb();
-
-  const { recentlyPlayedGames } = await READ_SEASON_FIELD_DATA(
-    leagueName,
-    seasonNumber,
-    "recent-results"
-  );
-  return JSON.stringify(recentlyPlayedGames);
-}
 
 async function recentScores() {
   const recentlyPlayedGames = JSON.parse(
--- app/@standings/page.js
+++ app/@standings/page.js
@@ -1,30 +1,11 @@
 import Standings from "@/components/client/Standings";
+import { getStandings } from "./(helpers)/get-standings";
+import { getDivisionsAndConferences } from "./(helpers)/get-divisions-and-conferences";
 import {
   DEFAULT_LEAGUE,
   MOST_RECENT_SEASON,
 } from "@/utils/constants/constants";
-import {
-  READ_SEASON_FIELD_DATA,
-  READ_SEASON_STANDINGS,
-} from "@/utils/constants/api_consts";
 import { connectToDb } from "@/utils/database";
-
-export const revalidate = 1;
-
-async function getStandings(leagueName, seasonNumber) {
-  const standings = await READ_SEASON_STANDINGS(leagueName, seasonNumber);
-
-  return JSON.stringify(standings);
-}
-
-async function getDivisionsAndConferences(leagueName, seasonNumber) {
-  const { divisionsAndConferences } = await READ_SEASON_FIELD_DATA(
-    leagueName,
-    seasonNumber,
-    "teams-conferences-and-divisions"
-  );
-  return JSON.stringify(divisionsAndConferences);
-}
 
 async function standingsPage() {
   await connectToDb();
