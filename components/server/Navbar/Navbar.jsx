8723e1baa54a8af8eef328153ee9a8c2c8f0ab63
ellis <lsp1@hotmail.com>
2023-10-30 19:29:49 -0600

    add navbar component

---
+++ components/server/Navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+
+function Navbar() {
+  const isAuthorized = false;
+
+  return (
+    <>
+      <nav className="flex flex-row gap-4 justify-between bg-slate-800 mb-2 p-2 sticky top-0">
+        <ul className="flex flex-row">
+          <li>
+            <Link href="/">
+              <Image
+                src="/images/general-logos/NHL95-sprites-banner.png"
+                alt="NHL 95 online"
+                width={200}
+                height={200}
+                style={{ height: "auto", width: "auto" }}
+                priority
+              />
+            </Link>
+          </li>
+        </ul>
+        <ul className="flex flex-row gap-4 items-center">
+          <li>
+            <Link href="standings">Standings</Link>
+          </li>
+          {isAuthorized && <Link href="dashboard">Dashboard</Link>}
+          {isAuthorized ? (
+            <li>
+              <Link href="sign-in">
+                <FaSignOutAlt className="text-blue-600" />
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link href="sign-in">
+                <FaSignInAlt className="text-orange-400" />
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
