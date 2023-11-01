d23261c0c3e046d792e316878fd51de45096a035
ellis <lsp1@hotmail.com>
2023-10-31 18:18:42 -0600

    navbar add submit and w logo

--- components/server/Navbar/Navbar.jsx
+++ components/server/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import Image from "next/image";
+import GeneralLogo from "../Logos/GeneralLogo";
+import LeagueLogo from "../Logos/LeagueLogo";
 
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 
@@ -8,24 +9,32 @@ function Navbar() {
 
   return (
     <>
-      <nav className="flex flex-row gap-4 justify-between bg-slate-800 mb-2 p-2 sticky top-0">
+      <nav className="flex flex-row gap-4 justify-between bg-slate-800 p-2 sticky top-0">
         <ul className="flex flex-row">
           <li>
             <Link href="/">
-              <Image
-                src="/images/general-logos/NHL95-sprites-banner.png"
-                alt="NHL 95 online"
+              <GeneralLogo
+                name="NHL95-sprites-banner"
                 width={200}
                 height={200}
-                style={{ height: "auto", width: "auto" }}
-                priority
               />
             </Link>
           </li>
         </ul>
         <ul className="flex flex-row gap-4 items-center">
           <li>
-            <Link href="standings">Standings</Link>
+            <Link href="/standings">
+              <LeagueLogo name="w" width="50" height="50" />
+            </Link>
+          </li>
+        </ul>
+        <ul className="flex flex-row gap-4 items-center">
+          <li>
+            <Link href="/submit-game" className="text-orange-400">
+              <button className=" rounded-md p-1 border-orange-400 border">
+                Submit Game
+              </button>
+            </Link>
           </li>
           {isAuthorized && <Link href="dashboard">Dashboard</Link>}
           {isAuthorized ? (
