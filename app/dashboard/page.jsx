3ac7e81e9d41edb33df2f9fa988b8b07166b0bb0
ellis <lsp1@hotmail.com>
2023-11-01 14:54:15 -0600

    dashbaoard temp message added

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,9 @@
 function Page() {
   return (
-    <div>
-      <h1 className="pt-4 text-4xl text-center">Dashboard</h1>
+    <div className="text-center leading-10">
+      <h1 className="pt-4 text-4xl mb-4">Dashboard</h1>
+      <p>You have been redirected to the future dashboard page</p>
+      <p>Admins will be able to edit the database from here</p>
     </div>
   );
 }
