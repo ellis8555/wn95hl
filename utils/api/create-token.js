01ea6f3f42961d789abdde15ad1038e218f9b49b
ellis <lsp1@hotmail.com>
2023-11-01 15:24:05 -0600

    rename SECRET_KEY with next_public

--- utils/api/create-token.js
+++ utils/api/create-token.js
@@ -1,7 +1,7 @@
 import { sign } from "jsonwebtoken";
 
 const createToken = (_id) => {
-  return sign({ _id }, process.env.SECRET_KEY, { expiresIn: "7d" });
+  return sign({ _id }, process.env.NEXT_PUBLIC_SECRET_KEY, { expiresIn: "7d" });
 };
 
 export default createToken;
