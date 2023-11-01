5ee42c24d486c24e5c75f0f8606b445b39614074
ellis <lsp1@hotmail.com>
2023-11-01 14:19:51 -0600

    game input re added to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,10 @@
+import GameInputForm from "@/components/client/GameInputForm";
+
+import {
+  DEFAULT_LEAGUE,
+  MOST_RECENT_SEASON,
+} from "@/utils/constants/constants";
+
 export const metadata = {
   title: "NHL95",
   description: "Test site for NHL95",
@@ -5,10 +12,12 @@ export const metadata = {
 
 export default async function Page() {
   return (
-    <div className="text-slate-300">
-      <h1 className="text-6xl text-center pt-4 center-div absolute">
-        NHL95 in development
-      </h1>
+    <div className="text-slate-300 center-div absolute">
+      <GameInputForm
+        leagueName={DEFAULT_LEAGUE}
+        seasonNumber={MOST_RECENT_SEASON}
+      />
+      <h1 className="text-6xl text-center pt-4">NHL95 in development</h1>
     </div>
   );
 }
