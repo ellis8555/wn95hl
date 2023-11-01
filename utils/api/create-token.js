ab0488d0faaeccd0dcbd0204643d3f1ee9696f6a
ellis <lsp1@hotmail.com>
2023-11-01 15:59:39 -0600

    rename secret env var

--- utils/api/create-token.js
+++ utils/api/create-token.js
@@ -1,7 +1,9 @@
 import { sign } from "jsonwebtoken";
 
 const createToken = (_id) => {
-  return sign({ _id }, process.env.NEXT_PUBLIC_SECRET_KEY, { expiresIn: "7d" });
+  return sign({ _id }, process.env.NEXT_PUBLIC_REACT_APP_SECRET_KEY, {
+    expiresIn: "7d",
+  });
 };
 
 export default createToken;
