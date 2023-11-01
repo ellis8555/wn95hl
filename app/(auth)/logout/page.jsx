9a6db08c5da5ef78562ccd3b7e88b535714fd14c
ellis <lsp1@hotmail.com>
2023-11-01 14:20:59 -0600

    logout segment added

---
+++ app/(auth)/logout/page.jsx
@@ -0,0 +1,9 @@
+import LogoutForm from "@/components/client/Logout/LogoutForm";
+
+export default async function Page() {
+  return (
+    <div className="text-slate-300">
+      <LogoutForm />
+    </div>
+  );
+}
