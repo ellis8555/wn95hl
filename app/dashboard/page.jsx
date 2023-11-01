82ce6f3fc1e7ca47e688e4b966fd650e63fc3f59
ellis <lsp1@hotmail.com>
2023-11-01 15:08:08 -0600

    dashboard temp message updated

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,7 +3,11 @@ function Page() {
     <div className="text-center leading-10">
       <h1 className="pt-4 text-4xl mb-4">Dashboard</h1>
       <p>You have been redirected to the future dashboard page</p>
-      <p>Admins will be able to edit the database from here</p>
+      <p>
+        Admins will be able to access this protected page and edit the database
+        from here
+      </p>
+      <p>You can sign out via the green arrow on the navigation bar</p>
     </div>
   );
 }
