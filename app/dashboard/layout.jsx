6b3cb99d0e69cc586742b34fc08ad2cd51715ad8
ellis <lsp1@hotmail.com>
2023-10-31 19:06:10 -0600

    create dashboard layout

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useEffect } from "react";
+import { useAuthorizationStatus } from "@/context/UserAuthContext";
+import { useRouter } from "next/navigation";
+
+function DashboardLayout({ children }) {
+  const { isAuthorized, setIsAuthorized } = useAuthorizationStatus();
+
+  const router = useRouter();
+
+  // validate user before displaying dashbard pate
+  useEffect(() => {
+    (async () => {
+      const { user, error } = await getUser();
+
+      if (error) {
+        router.push("/sign-in");
+        return;
+      }
+      setIsAuthorized(true);
+    })();
+  }, [router]);
+
+  if (!isAuthorized) {
+    return <p>Authorizing...</p>;
+  }
+
+  return (
+    <div>
+      <p>This is the dashboard layout page!</p>
+      {children}
+    </div>
+  );
+}
+
+async function getUser() {
+  try {
+    const response = await fetch("/api/admin/auth");
+
+    if (!response.ok) {
+      setIsAuthorized(false);
+    }
+
+    const data = await response.json();
+
+    return {
+      user: data,
+      error: null,
+    };
+  } catch (error) {
+    return {
+      user: null,
+      error: error.message,
+    };
+  }
+}
+
+export default DashboardLayout;
