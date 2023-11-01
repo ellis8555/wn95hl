e184bf4d4858577089921bb8a24123591bcd4b0f
ellis <lsp1@hotmail.com>
2023-10-31 18:56:04 -0600

    create loginform component

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,9 @@
+import LogInForm from "@/components/client/login/LoginInForm";
+
+export default async function Page() {
+  return (
+    <div className="text-slate-300">
+      <LogInForm />
+    </div>
+  );
+}
