332a93184fd5c6b35c5b070cd83b6a872247ff0f
ellis <lsp1@hotmail.com>
2023-11-01 22:46:49 -0600

    add fetch to api const methods

--- utils/constants/data-calls/api_calls.js
+++ utils/constants/data-calls/api_calls.js
@@ -1,23 +1,13 @@
 import { DOMAIN } from "../connections";
 
-export const SUBMIT_GAME_RESULT = async (gameData) => {
-  const url = `${DOMAIN}/api/game-result`;
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(gameData),
-  });
+// 1. GET_LEAGUE_DATA
+// 2. GET_API_WITH_PARAMS
+// 3. POST_JSON_TO_API
+// 4. GET_API
 
-  if (!response.ok) {
-    const responseError = await response.json();
-    return new Error(responseError.message);
-  }
-
-  return response;
-};
-
+///////////////////
+// GET_LEAGUE_DATA
+///////////////////
 export const GET_LEAGUE_DATA = async (leagueName, seasonNumber, ...fields) => {
   let fieldsToGet = "";
 
@@ -31,10 +21,57 @@ export const GET_LEAGUE_DATA = async (leagueName, seasonNumber, ...fields) => {
     },
   });
 
-  if (!leagueData.ok) {
-    const errorMessage = await leagueData.json();
-    return new Error(errorMessage.message);
-  }
+  await checkResponseOk(leagueData);
 
   return leagueData;
 };
+
+//////////////////////
+// GET_API_WITH_PARAMS
+//////////////////////
+export const GET_API_WITH_PARAMS = async (segments, params) => {
+  const url = `${DOMAIN}/api/${segments}?${params}`;
+  const response = await fetch(url);
+
+  await checkResponseOk(response);
+
+  return await response.json();
+};
+
+///////////////////
+// POST_JSON_TO_API
+///////////////////
+export const POST_JSON_TO_API = async (segments, body) => {
+  const url = `${DOMAIN}/api/${segments}`;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  await checkResponseOk(response);
+
+  return await response.json();
+};
+
+////////////
+// GET_API
+////////////
+export const GET_API = async (segments) => {
+  const url = `${DOMAIN}/api/${segments}`;
+  const response = await fetch(url);
+
+  await checkResponseOk(response);
+
+  return await response.json();
+};
+
+async function checkResponseOk(res) {
+  if (!res.ok) {
+    const errorMessage = await res.json();
+    return new Error(errorMessage.message);
+  }
+  return;
+}
