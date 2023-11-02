771ffc0559851b92b3697e9251cbf5ce3a444e6a
ellis <lsp1@hotmail.com>
2023-11-02 15:54:27 -0600

    cookies from request object auth api

--- app/api/(authorization)/auth/route.js
+++ app/api/(authorization)/auth/route.js
@@ -1,25 +1,16 @@
-import { cookies } from "next/headers";
 import { verify } from "jsonwebtoken";
 import { AUTH_COOKIE } from "@/utils/constants/constants";
 import nextResponse from "@/utils/api/next-response";
 
-export async function GET() {
-  const cookieStore = cookies();
-
-  const token = cookieStore.get(AUTH_COOKIE);
-
-  if (!token) {
-    return nextResponse({ message: "Unauthorized" }, 400, "GET");
-  }
-
-  const { value } = token;
-  const secret = process.env.SECRET_KEY;
+export const GET = async (request) => {
+  const token = request.cookies.get(AUTH_COOKIE);
 
   try {
+    const { value } = token;
+    const secret = process.env.SECRET_KEY;
     const user = verify(value, secret);
-
     return nextResponse({ message: `User id: ${user._id}` }, 200, "GET");
   } catch (error) {
     return nextResponse({ message: "Not authorized" }, 400, "GET");
   }
-}
+};
