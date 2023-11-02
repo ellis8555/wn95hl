6bc18d539d2715628d2bfd796929d2ea0b81868a
ellis <lsp1@hotmail.com>
2023-11-01 21:21:25 -0600

    list align dashboard comments

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,14 +1,18 @@
 function Page() {
   return (
-    <div className="text-center leading-10">
-      <h1 className="pt-4 text-4xl mb-4">Dashboard</h1>
-      <p>You have been redirected to the future dashboard page</p>
-      <p>
-        Admins will be able to access this protected page and edit the database
-        from here
-      </p>
-      <p>You can sign out via the green arrow on the navigation bar</p>
-    </div>
+    <>
+      <h1 className="text-4xl text-center pt-4 mb-4">Dashboard</h1>
+      <div className="text-center">
+        <ul className="inline-block ps-2 sm:p-0 text-left list-none">
+          <li>You have been redirected to the future dashboard page</li>
+          <li>
+            Admins will be able to access this protected page and edit the
+            database from here
+          </li>
+          <li>You can sign out via the green arrow on the navigation bar</li>
+        </ul>
+      </div>
+    </>
   );
 }
 
