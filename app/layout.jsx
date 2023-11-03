0d8a6665c8502b09f46140014a05c5a1f63cc2ed
ellis <lsp1@hotmail.com>
2023-11-03 13:52:24 -0600

    index layout remove parallel route

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,13 @@ import "./globals.css";
 import Navbar from "@/components/client/Navbar/Navbar";
 import { LeagueStandingsProvider } from "@/context/FullLeagueStandingsContext";
 import { UserAuthContextProvider } from "@/context/UserAuthContext";
+import Boxscore from "@/components/client/Boxscore/Boxscore";
+import GameResultScore from "@/components/client/Boxscore/GameResultScore";
+import { getRecentGameResults } from "./(helpers)/get-recent-game-results";
+import {
+  DEFAULT_LEAGUE,
+  MOST_RECENT_SEASON,
+} from "@/utils/constants/constants";
 
 export const metadata = {
   title: "NHL 95",
@@ -10,13 +17,22 @@ export const metadata = {
 
 export const revalidate = 0;
 
-export default function RootLayout({ children, recentScores }) {
+export default async function RootLayout({ children }) {
+  const recentlyPlayedGames = JSON.parse(
+    await getRecentGameResults(DEFAULT_LEAGUE, MOST_RECENT_SEASON)
+  );
+
   return (
     <html lang="en">
       <body className="bg-slate-700">
         <UserAuthContextProvider>
           <LeagueStandingsProvider>
-            {recentScores}
+            <div className="text-slate-300 mb-[1px]">
+              <Boxscore recentGameResults={recentlyPlayedGames} />
+              <div className="sm:hidden">
+                <GameResultScore recentGameResults={recentlyPlayedGames} />
+              </div>
+            </div>
             <Navbar />
             {children}
           </LeagueStandingsProvider>
