e6f72322f3dfb67b1dbbf7ed7041dbc294dccada
ellis <lsp1@hotmail.com>
2023-11-05 08:35:16 -0700

    create edit boxscore page

---
+++ app/(boxscore)/edit-boxscore/page.jsx
@@ -0,0 +1,10 @@
+async function page() {
+  return (
+    <div className="text-center text-slate-300">
+      <h1 className="text-xl py-3 md:text-3xl md:py-6">Boxscore Dashboard</h1>
+      <p>Edit a particular games data coming</p>
+    </div>
+  );
+}
+
+export default page;
