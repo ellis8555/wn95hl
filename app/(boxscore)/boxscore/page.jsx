7b8b92cee4ee2d166d19df566d3cc86a6cfe1d9e
ellis <lsp1@hotmail.com>
2023-11-05 08:35:29 -0700

    create boxscore page

---
+++ app/(boxscore)/boxscore/page.jsx
@@ -0,0 +1,10 @@
+async function page() {
+  return (
+    <div className="text-center text-slate-300">
+      <h1 className="text-xl py-3 md:text-3xl md:py-6">Boxscore</h1>
+      <p>Game stats will be displayed here in the future</p>
+    </div>
+  );
+}
+
+export default page;
