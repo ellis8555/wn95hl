de4c4f3b49629f3ac0b61d8dd09e610ae34cb53a
ellis <lsp1@hotmail.com>
2023-11-05 08:59:03 -0700

    link to edit a game added

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,15 +1,15 @@
+import Link from "next/link";
+
 function Page() {
   return (
     <>
       <h1 className="text-4xl text-center pt-4 mb-4">Dashboard</h1>
       <div className="text-center">
         <ul className="inline-block ps-2 sm:p-0 text-left list-none">
-          <li>You have been redirected to the future dashboard page</li>
-          <li>
-            Admins will be able to access this protected page and edit the
-            database from here
-          </li>
           <li>You can sign out via the green arrow on the navigation bar</li>
+          <li className="text-green-400">
+            <Link href="/recent-scores">Edit a game</Link>
+          </li>
         </ul>
       </div>
     </>
