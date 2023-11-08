e743809fe1e345805c3ac0245c0215c71780ec64
ellis <lsp1@hotmail.com>
2023-11-07 18:54:36 -0700

    boxscore ticker dynamic effect

--- components/client/Boxscore/Boxscore.jsx
+++ components/client/Boxscore/Boxscore.jsx
@@ -1,11 +1,20 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useFullLeagueStandings } from "@/context/FullLeagueStandingsContext";
+import { GET_LEAGUE_DATA } from "@/utils/constants/data-calls/api_calls";
+import { MOST_RECENT_SEASON } from "@/utils/constants/constants";
 import Ticker from "@/components/server/Boxscore/Ticker";
 
 function Boxscore({ recentGameResults }) {
-  const [recentGamesPlayed, setrecentGamesPlayed] = useState(recentGameResults);
+  // used to update the ticker scores on user selecting various leagues
+  const path = usePathname();
+  const params = useSearchParams();
+
+  // check if user has selected a league to change score ticker
+
+  const [recentGamesPlayed, setRecentGamesPlayed] = useState(recentGameResults);
   const gamesDate = useRef();
   const gamesDay = useRef();
   const [gameDates, setGameDates] = useState([]);
@@ -13,12 +22,49 @@ function Boxscore({ recentGameResults }) {
   const { clientRecentlyPlayedGames, refreshTheStandings } =
     useFullLeagueStandings();
 
+  // detect league change to display correct leagues ticker scores
+  useEffect(() => {
+    (async () => {
+      if (path != "/") {
+        // leagues linked page uses paths
+        // split the url
+        const urlParts = path.split("/");
+        // the last segment contains the league name
+        const leagueName = urlParts[urlParts.length - 1];
+        // fetch that leagues most recent games
+        const { recentlyPlayedGames } = await GET_LEAGUE_DATA(
+          leagueName,
+          MOST_RECENT_SEASON,
+          "recent-results"
+        );
+        setRecentGamesPlayed(recentlyPlayedGames);
+        return;
+      }
+      // if recent score ticker is requested to change from home page
+      if (path == "/" && params.has("league")) {
+        console.log("inside of params scores");
+        // get the leagues name
+        const leagueName = params.get("league");
+        // fetch that leagues most recent games
+        const { recentlyPlayedGames } = await GET_LEAGUE_DATA(
+          leagueName,
+          MOST_RECENT_SEASON,
+          "recent-results"
+        );
+        setRecentGamesPlayed(recentlyPlayedGames);
+        return;
+      }
+    })();
+  }, [path, params]);
+
+  // update ticker when a new game file is uploaded
   useEffect(() => {
     if (refreshTheStandings) {
-      setrecentGamesPlayed(clientRecentlyPlayedGames);
+      setRecentGamesPlayed(clientRecentlyPlayedGames);
     }
   }, [clientRecentlyPlayedGames]);
 
+  // displays dates for games submitted on different dates
   useEffect(() => {
     const gameDatesArray = [0];
 
