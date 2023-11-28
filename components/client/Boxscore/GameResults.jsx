619dcb815747c067a22b68296d9b7077ff867114
ellis <lsp1@hotmail.com>
2023-11-28 10:45:47 -0700

    create gameResults component

---
+++ components/client/Boxscore/GameResults.jsx
@@ -0,0 +1,67 @@
+"use client";
+
+///////////////////////////////////
+// Displays recent scores on mobile
+///////////////////////////////////
+
+import { useEffect, useRef, useState } from "react";
+import { useFullLeagueStandings } from "@/context/FullLeagueStandingsContext";
+import GameResultScore from "./GameResultScore";
+
+function GameResults({ recentGameResults }) {
+  const [recentGamesPlayed, setRecentGamesPlayed] = useState(recentGameResults);
+  const gamesDate = useRef();
+  const gamesDay = useRef();
+  const [gameDates, setGameDates] = useState([]);
+
+  const { clientRecentlyPlayedGames, refreshTheStandings } =
+    useFullLeagueStandings();
+
+  // update recent scores when a new game file is uploaded
+  useEffect(() => {
+    if (refreshTheStandings) {
+      setRecentGamesPlayed(clientRecentlyPlayedGames);
+    }
+  }, [clientRecentlyPlayedGames]);
+
+  // displays dates for games submitted on different dates
+  useEffect(() => {
+    const gameDatesArray = [0];
+    if (recentGamesPlayed.length > 0) {
+      const firstGameTimestamp =
+        recentGamesPlayed[0].otherGameStats.submittedAt ||
+        new Date("2022-09-17");
+      const firstGameTimestampInTicker = Date.parse(firstGameTimestamp);
+      gamesDate.current = new Date(firstGameTimestampInTicker);
+      gamesDay.current = gamesDate.current.getDate();
+      recentGamesPlayed.forEach((game, index) => {
+        const gameTimestampTicker = Date.parse(
+          game.otherGameStats.submittedAt || new Date("1970-01-01")
+        );
+        const thisGamesDate = new Date(gameTimestampTicker);
+        const thisGamesDay = thisGamesDate.getDate();
+
+        if (thisGamesDay != gamesDay.current) {
+          gameDatesArray.push(index);
+          gamesDay.current = thisGamesDay;
+        }
+      });
+      setGameDates(gameDatesArray);
+    }
+  }, [recentGamesPlayed]);
+
+  return (
+    <div>
+      {recentGamesPlayed.map((game, index) => (
+        <GameResultScore
+          recentGameResult={game}
+          gameDateIndexes={gameDates}
+          index={index}
+          key={index}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default GameResults;
