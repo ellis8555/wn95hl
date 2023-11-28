1aedff9a67a49feab2431d1ba97e4787e5b4793d
ellis <lsp1@hotmail.com>
2023-11-28 10:45:06 -0700

    game results page GameResults comp added

--- app/recent-scores/page.jsx
+++ app/recent-scores/page.jsx
@@ -1,4 +1,4 @@
-import GameResultScore from "@/components/client/Boxscore/GameResultScore";
+import GameResults from "@/components/client/Boxscore/GameResults";
 import { getRecentGameResults } from "../(helpers)/get-recent-game-results";
 
 import {
@@ -8,51 +8,17 @@ import {
 
 export const revalidate = 0;
 
-// fetch most recent games
-const recentlyPlayedGames = JSON.parse(
-  await getRecentGameResults(DEFAULT_LEAGUE, MOST_RECENT_SEASON)
-);
-
-// determine which games were submitted on different dates
-let gamesDate;
-let gamesDay;
-// game that has a different date than the previous game will have it's index pushed into this array
-const gameDates = [0];
-
-if (recentlyPlayedGames.length > 0) {
-  const firstGameTimestamp =
-    recentlyPlayedGames[0].otherGameStats.submittedAt || new Date("2022-09-17");
-  const firstGameTimestampInTicker = Date.parse(firstGameTimestamp);
-  gamesDate = new Date(firstGameTimestampInTicker);
-  gamesDay = gamesDate.getDate();
-  recentlyPlayedGames.forEach((game, index) => {
-    const gameTimestampTicker = Date.parse(
-      game.otherGameStats.submittedAt || new Date("1970-01-01")
-    );
-    const thisGamesDate = new Date(gameTimestampTicker);
-    const thisGamesDay = thisGamesDate.getDate();
-
-    if (thisGamesDay != gamesDay) {
-      gameDates.push(index);
-      gamesDay = thisGamesDay;
-    }
-  });
-}
-
 async function page() {
+  // fetch most recent games
+  const recentlyPlayedGames = JSON.parse(
+    await getRecentGameResults(DEFAULT_LEAGUE, MOST_RECENT_SEASON)
+  );
   return (
     <div>
       <h1 className="text-xl text-center text-slate-300 my-3 md:text-3xl md:my-6">
         Recent Scores
       </h1>
-      {recentlyPlayedGames.map((gameResult, index) => (
-        <GameResultScore
-          key={index}
-          index={index}
-          recentGameResult={gameResult}
-          gameDateIndexes={gameDates}
-        />
-      ))}
+      <GameResults recentGameResults={recentlyPlayedGames} />
     </div>
   );
 }
