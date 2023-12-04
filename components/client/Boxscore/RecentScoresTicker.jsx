61a2f26ba3fad49514434687a243a154ca499ef4
ellis <lsp1@hotmail.com>
2023-12-04 09:26:50 -0700

    navigation added to score ticker

--- components/client/Boxscore/RecentScoresTicker.jsx
+++ components/client/Boxscore/RecentScoresTicker.jsx
@@ -1,20 +1,33 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useFullLeagueStandings } from "@/context/FullLeagueStandingsContext";
-import { GET_LEAGUE_DATA } from "@/utils/constants/data-calls/api_calls";
-import { MOST_RECENT_SEASON } from "@/utils/constants/constants";
+import {
+  GET_API_WITH_PARAMS,
+  GET_LEAGUE_DATA,
+} from "@/utils/constants/data-calls/api_calls";
+import {
+  DEFAULT_LEAGUE,
+  MOST_RECENT_SEASON,
+} from "@/utils/constants/constants";
+import { FaCircleArrowLeft } from "react-icons/fa6";
+import { FaCircleArrowRight } from "react-icons/fa6";
 import Ticker from "@/components/server/Boxscore/Ticker";
+
+const HOW_MANY_GAME_RESULTS = 8;
 
 function RecentScoresTicker({ recentGameResults, leagueName, seasonNumber }) {
   // used to update the ticker scores on user selecting various leagues
   const path = usePathname();
   const params = useSearchParams();
 
-  // check if user has selected a league to change score ticker
-
   const [recentGamesPlayed, setRecentGamesPlayed] = useState(recentGameResults);
+  const [totalGamesPlayed, setTotalGamesPlayed] = useState();
+  const indexOfCurrentGamesDisplayed = useRef();
+  const [displayPreviousGamesArrow, setDisplayPreviousGamesArrow] =
+    useState(true);
+  const [displayNextGamesArrow, setDisplayNextGamesArrow] = useState(false);
   const gamesDate = useRef();
   const gamesDay = useRef();
   const [gameDates, setGameDates] = useState([]);
@@ -32,11 +45,24 @@ function RecentScoresTicker({ recentGameResults, leagueName, seasonNumber }) {
         // the last segment contains the league name
         const leagueName = urlParts[urlParts.length - 1];
         // fetch that leagues most recent games
-        const { recentlyPlayedGames } = await GET_LEAGUE_DATA(
-          leagueName,
-          MOST_RECENT_SEASON,
-          "recent-results"
-        );
+        const { recentlyPlayedGames, totalGamesSubmitted } =
+          await GET_LEAGUE_DATA(
+            leagueName,
+            MOST_RECENT_SEASON,
+            "recent-results"
+          );
+        setTotalGamesPlayed(totalGamesSubmitted);
+        indexOfCurrentGamesDisplayed.current = totalGamesSubmitted - 8;
+        if (indexOfCurrentGamesDisplayed.current < 0) {
+          indexOfCurrentGamesDisplayed.current = 0;
+        }
+        if (totalGamesPlayed <= 8) {
+          setDisplayPreviousGamesArrow(false);
+          setDisplayNextGamesArrow(false);
+        } else {
+          setDisplayPreviousGamesArrow(true);
+          setDisplayNextGamesArrow(false);
+        }
         setRecentGamesPlayed(recentlyPlayedGames);
         return;
       }
@@ -45,16 +71,51 @@ function RecentScoresTicker({ recentGameResults, leagueName, seasonNumber }) {
         // get the leagues name
         const leagueName = params.get("league");
         // fetch that leagues most recent games
-        const { recentlyPlayedGames } = await GET_LEAGUE_DATA(
-          leagueName,
-          MOST_RECENT_SEASON,
-          "recent-results"
-        );
+        const { recentlyPlayedGames, totalGamesSubmitted } =
+          await GET_LEAGUE_DATA(
+            leagueName,
+            MOST_RECENT_SEASON,
+            "recent-results"
+          );
+        setTotalGamesPlayed(totalGamesSubmitted);
+        indexOfCurrentGamesDisplayed.current = totalGamesSubmitted - 8;
+        if (indexOfCurrentGamesDisplayed.current < 0) {
+          indexOfCurrentGamesDisplayed.current = 0;
+        }
+        if (totalGamesPlayed <= 8) {
+          setDisplayPreviousGamesArrow(false);
+          setDisplayNextGamesArrow(false);
+        } else {
+          setDisplayPreviousGamesArrow(true);
+          setDisplayNextGamesArrow(false);
+        }
+        setRecentGamesPlayed(recentlyPlayedGames);
+        return;
+      } else {
+        // display on original home page landing
+        const { recentlyPlayedGames, totalGamesSubmitted } =
+          await GET_LEAGUE_DATA(
+            DEFAULT_LEAGUE,
+            MOST_RECENT_SEASON,
+            "recent-results"
+          );
+        setTotalGamesPlayed(totalGamesSubmitted);
+        indexOfCurrentGamesDisplayed.current = totalGamesSubmitted - 8;
+        if (indexOfCurrentGamesDisplayed.current < 0) {
+          indexOfCurrentGamesDisplayed.current = 0;
+        }
+        if (totalGamesPlayed <= 8) {
+          setDisplayPreviousGamesArrow(false);
+          setDisplayNextGamesArrow(false);
+        } else {
+          setDisplayPreviousGamesArrow(true);
+          setDisplayNextGamesArrow(false);
+        }
         setRecentGamesPlayed(recentlyPlayedGames);
         return;
       }
     })();
-  }, [path, params]);
+  }, [refreshTheStandings]);
 
   // update ticker when a new game file is uploaded
   useEffect(() => {
@@ -90,10 +151,70 @@ function RecentScoresTicker({ recentGameResults, leagueName, seasonNumber }) {
     }
   }, [recentGamesPlayed]);
 
+  // handle previous games button
+  async function updateTickerPreviousGames() {
+    // hide if less than 8 games submitted
+    if (totalGamesPlayed < 8) {
+      indexOfCurrentGamesDisplayed.current = 0;
+      setDisplayPreviousGamesArrow(false);
+      return;
+    }
+    if (indexOfCurrentGamesDisplayed.current <= 8) {
+      indexOfCurrentGamesDisplayed.current = 0;
+
+      setDisplayPreviousGamesArrow(false);
+      setDisplayNextGamesArrow(true);
+    }
+    if (indexOfCurrentGamesDisplayed.current > 8) {
+      indexOfCurrentGamesDisplayed.current -= 8;
+      setDisplayPreviousGamesArrow(true);
+      setDisplayNextGamesArrow(true);
+    }
+    const paramsList = `league=${leagueName}&season-number=${seasonNumber}&beginning-index=${indexOfCurrentGamesDisplayed.current}&how-many-games=${HOW_MANY_GAME_RESULTS}`;
+    const games = await GET_API_WITH_PARAMS("recent-games", paramsList);
+    setRecentGamesPlayed(games);
+  }
+
+  // handle next games button
+  async function updateTickerNextGames() {
+    // hide if less than 8 games submitted
+    if (totalGamesPlayed < 8) {
+      indexOfCurrentGamesDisplayed.current = 0;
+      setDisplayNextGamesArrow(false);
+      return;
+    }
+    const diff = totalGamesPlayed - indexOfCurrentGamesDisplayed.current;
+    if (diff <= 15) {
+      indexOfCurrentGamesDisplayed.current = totalGamesPlayed - 8;
+
+      setDisplayNextGamesArrow(false);
+      if (indexOfCurrentGamesDisplayed.current > 7) {
+        setDisplayPreviousGamesArrow(true);
+      }
+    } else {
+      indexOfCurrentGamesDisplayed.current += 8;
+      setDisplayNextGamesArrow(true);
+      if (indexOfCurrentGamesDisplayed.current > 7) {
+        setDisplayPreviousGamesArrow(true);
+      }
+    }
+    const paramsList = `league=${leagueName}&season-number=${seasonNumber}&beginning-index=${indexOfCurrentGamesDisplayed.current}&how-many-games=${HOW_MANY_GAME_RESULTS}`;
+    const games = await GET_API_WITH_PARAMS("recent-games", paramsList);
+    setRecentGamesPlayed(games);
+  }
+
   return (
     <>
       {/* entire score ticker container */}
       <div className="hidden lg:flex flex-row justify-center gap-[1px]">
+        {displayPreviousGamesArrow && (
+          <div className="flex items-center pr-1">
+            <FaCircleArrowLeft
+              className="text-xl cursor-pointer hover:text-orange-400"
+              onClick={updateTickerPreviousGames}
+            />
+          </div>
+        )}
         {recentGamesPlayed.map((game, index) => (
           <Ticker
             gameData={game}
@@ -104,9 +225,17 @@ function RecentScoresTicker({ recentGameResults, leagueName, seasonNumber }) {
             key={index}
           />
         ))}
+        {displayNextGamesArrow && (
+          <div className="flex items-center pl-1">
+            <FaCircleArrowRight
+              className="text-xl cursor-pointer hover:text-orange-400"
+              onClick={updateTickerNextGames}
+            />
+          </div>
+        )}
       </div>
     </>
   );
 }
 
-export default RecentScoresTicker;
+export default React.memo(RecentScoresTicker);
