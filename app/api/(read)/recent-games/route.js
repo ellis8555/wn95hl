3e9813660d068a217a2238a133cbdc502fb62671
ellis <lsp1@hotmail.com>
2023-12-04 09:26:19 -0700

    recent-games api created

---
+++ app/api/(read)/recent-games/route.js
@@ -0,0 +1,44 @@
+import { connectToDb } from "@/utils/database";
+import nextResponse from "@/utils/api/next-response";
+import { LEAGUE_SCHEMA_SWITCH } from "@/utils/constants/data-calls/db_calls";
+
+export const GET = async (req, res) => {
+  const { searchParams } = new URL(req.url);
+  let leagueName = searchParams.get("league");
+  let seasonNumber = searchParams.get("season-number");
+  let beginningIndex = +searchParams.get("beginning-index");
+  let howManyGamesToGet = +searchParams.get("how-many-games");
+
+  // grab correct league schema in order to get the correct seasons data
+  const League = await LEAGUE_SCHEMA_SWITCH(leagueName);
+  // if no seasonNumber parameter set to most recent season
+  if (!seasonNumber) {
+    const seasons = await League.find({}, "seasonNumber");
+    const seasonsList = seasons.map((season) => {
+      return season.seasonNumber;
+    });
+    seasonNumber = Math.max(...seasonsList).toString();
+  }
+
+  try {
+    await connectToDb();
+
+    // get this seasons document
+    const seasonData = await League.findOne({ seasonNumber });
+
+    if (!seasonData) {
+      throw new Error(`Season ${seasonNumber} has not been found`);
+    }
+
+    // extract teams array of team objects
+    const selectedGames = await League.getSelectedGames(
+      seasonNumber,
+      beginningIndex,
+      howManyGamesToGet
+    );
+
+    return nextResponse(selectedGames, 200, "GET");
+  } catch (error) {
+    return nextResponse({ message: error.message }, 500, "GET");
+  }
+};
