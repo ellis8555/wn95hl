0b2f7be9ddf4832bc557797c5db42fe99d685e87
ellis <lsp1@hotmail.com>
2023-12-09 10:32:10 -0700

    create READ_SEASON_GAME_RESULTS

--- utils/constants/data-calls/db_calls.js
+++ utils/constants/data-calls/db_calls.js
@@ -1,5 +1,5 @@
 import W_Season from "@/schemas/season/w_season";
-import { DEFAULT_LEAGUE } from "../constants";
+import { DEFAULT_LEAGUE, HOW_MANY_GAME_RESULTS } from "../constants";
 
 export async function LEAGUE_SCHEMA_SWITCH(leagueName) {
   switch (leagueName) {
@@ -46,6 +46,26 @@ export async function READ_SEASON_FIELD_DATA(
   const fieldData = {};
   await League.getFieldData(seasonNumber, requestedField, fieldData);
   return fieldData;
+}
+
+//////////////////////////////////////
+// api method takes in 4th param index
+//////////////////////////////////////
+// returns an object
+export async function READ_SEASON_GAME_RESULTS(
+  leagueName,
+  seasonNumber,
+  indexNumber
+) {
+  const League = await getLeagueAndSeason(leagueName, seasonNumber);
+  const { selectedGames, totalGamesSubmitted } = await League.getSelectedGames(
+    seasonNumber,
+    indexNumber,
+    HOW_MANY_GAME_RESULTS
+  );
+
+  const gameData = { selectedGames, totalGamesSubmitted };
+  return gameData;
 }
 
 ////////////////////////////////////////////
