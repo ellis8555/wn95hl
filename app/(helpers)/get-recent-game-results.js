ab147a2932c80e9d12a0a48e964c832b2752ce2d
ellis <lsp1@hotmail.com>
2023-12-09 10:38:54 -0700

    remove next cache method

--- app/(helpers)/get-recent-game-results.js
+++ app/(helpers)/get-recent-game-results.js
@@ -1,13 +1,9 @@
-import { cache } from "react";
 import { connectToDb } from "@/utils/database";
 import { READ_SEASON_FIELD_DATA } from "@/utils/constants/data-calls/db_calls";
 
 export const revalidate = 0;
 
-export const getRecentGameResults = cache(async function (
-  leagueName,
-  seasonNumber
-) {
+export const getRecentGameResults = async function (leagueName, seasonNumber) {
   await connectToDb();
 
   const { recentlyPlayedGames, totalGamesSubmitted } =
@@ -18,4 +14,4 @@ export const getRecentGameResults = cache(async function (
     totalGamesSubmitted,
   };
   return JSON.stringify(gameData);
-});
+};
