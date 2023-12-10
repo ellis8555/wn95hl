debf0b91d1eeaa0e0b3130a1e9d991425ed1c7cd
ellis <lsp1@hotmail.com>
2023-12-10 09:53:42 -0700

    TeamLogo surrounded with Link tag

--- components/server/standings/TeamLogo.jsx
+++ components/server/standings/TeamLogo.jsx
@@ -1,15 +1,18 @@
+import Link from "next/link";
 import Image from "next/image";
 
 function TeamLogo({ name, width, height }) {
   return (
-    <Image
-      src={`/images/team-logos/${name}.png`}
-      width={width}
-      height={height}
-      style={{ width: "auto", height: "auto" }}
-      alt={name}
-      quality={100}
-    />
+    <Link href={`/team/${name}`}>
+      <Image
+        src={`/images/team-logos/${name}.png`}
+        width={width}
+        height={height}
+        style={{ width: "auto", height: "auto" }}
+        alt={name}
+        quality={100}
+      />
+    </Link>
   );
 }
 
