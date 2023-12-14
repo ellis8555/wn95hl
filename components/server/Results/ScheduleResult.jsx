ea05f403b5a54eb064b62a1a0b908741bef405db
ellis <lsp1@hotmail.com>
2023-12-14 08:49:54 -0700

    create schedule result

---
+++ components/server/Results/ScheduleResult.jsx
@@ -0,0 +1,44 @@
+import FinalResult from "./FinalResult";
+import TeamLogo from "../standings/TeamLogo";
+
+const widthHeight = 40;
+
+function ScheduleResult({ teamAcronym, games }) {
+  return (
+    <div className="flex flex-col">
+      {games.map((game) => {
+        const goalsByEachTeam = [];
+        goalsByEachTeam.push(game["homeTeamGameStats"]["HomeGOALS"]);
+        goalsByEachTeam.push(game["awayTeamGameStats"]["AwayGOALS"]);
+        goalsByEachTeam.sort((a, b) => b - a);
+        return (
+          <div key={game._id} className="flex flex-row gap-4">
+            {game["otherGameStats"]["homeTeam"] === teamAcronym ? (
+              <div className="flex flex-row items-center gap-4">
+                <FinalResult teamAcronym={teamAcronym} game={game} />
+                <TeamLogo
+                  name={game["otherGameStats"]["awayTeam"]}
+                  width={widthHeight}
+                  height={widthHeight}
+                />
+                <div className="text-lg">{`${goalsByEachTeam[0]} - ${goalsByEachTeam[1]}`}</div>
+              </div>
+            ) : (
+              <div className="flex flex-row items-center gap-4">
+                <FinalResult teamAcronym={teamAcronym} game={game} />
+                <TeamLogo
+                  name={game["otherGameStats"]["homeTeam"]}
+                  width={widthHeight}
+                  height={widthHeight}
+                />
+                <div className="text-lg">{`${goalsByEachTeam[0]} - ${goalsByEachTeam[1]}`}</div>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ScheduleResult;
