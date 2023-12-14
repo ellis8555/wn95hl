57ae2677eeac8d2cfb95aa0ded27a97b0b5d34ed
ellis <lsp1@hotmail.com>
2023-12-14 08:50:13 -0700

    create final result indicator

---
+++ components/server/Results/FinalResult.jsx
@@ -0,0 +1,32 @@
+function Result({ teamAcronym, game }) {
+  let bgColor;
+  let result;
+  if (game.otherGameStats.losingTeam == teamAcronym) {
+    if (game.otherGameStats.overtimeLossTeam == teamAcronym) {
+      bgColor = "bg-orange-600";
+      result = "OTL";
+    } else {
+      bgColor = "bg-red-600";
+      result = "L";
+    }
+  } else if (
+    game.otherGameStats.homeTeamPoints == 1 &&
+    game.otherGameStats.awayTeamPoints == 1
+  ) {
+    bgColor = "bg-slate-700";
+    result = "T";
+  } else {
+    bgColor = "bg-green-600";
+    result = "W";
+  }
+  return (
+    <div
+      key={game._id}
+      className={`flex justify-center border border-slate-300 p-0 my-4 w-10 ${bgColor}`}
+    >
+      {result}
+    </div>
+  );
+}
+
+export default Result;
