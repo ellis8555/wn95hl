6bd5b4a08cef133c6272a5bebbc1500f68eeef0a
ellis <lsp1@hotmail.com>
2023-12-17 16:11:25 -0700

    capitalize Loading method in route

---
+++ app/team/[league]/[season-number]/[team-acronym]/loading.jsx
@@ -0,0 +1,9 @@
+// text is same color as bg to show blank intentionally
+
+export default function Loading() {
+  return (
+    <div className="text-slate-700 text-center text-3xl mt-8">
+      Loading Team...
+    </div>
+  );
+}
