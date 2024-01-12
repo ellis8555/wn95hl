8a39f5fea76e7061d5576579f04e3e4cd217a83c
ellis <lsp1@hotmail.com>
2024-01-12 13:00:26 -0700

    db connectivity messages added

--- utils/database.js
+++ utils/database.js
@@ -3,25 +3,26 @@ import { DB_CONNECT } from "./constants/connections";
 
 const connectedMessage = "Connected to wn95hl database";
 const disconnectedMessage = "Database connection closed.";
+const notYetLocated = "(yet to be located)";
 
-export const connectToDb = async () => {
+export const connectToDb = async (callingLocation = notYetLocated) => {
   try {
     await mongoose.connect(DB_CONNECT, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    console.log(connectedMessage);
+    console.log(`${connectedMessage} ${callingLocation}`);
   } catch (error) {
     console.log(error);
     throw new Error(`An error has occured with the database..`);
   }
 };
 
-export const closeDbConnection = async () => {
+export const closeDbConnection = async (callingLocation = notYetLocated) => {
   try {
     await mongoose.disconnect();
-    console.log(disconnectedMessage);
+    console.log(`${disconnectedMessage} ${callingLocation}`);
   } catch (error) {
     console.log(error);
   }
