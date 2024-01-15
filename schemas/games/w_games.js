9b29b09a67a5e1d2bf50748a0df57efbe2c785be
ellis <lsp1@hotmail.com>
2024-01-15 01:07:54 -0700

    add static methods on game schema

--- schemas/games/w_games.js
+++ schemas/games/w_games.js
@@ -9,7 +9,10 @@ const W_GamesSchema = new Schema({
 
 // statics list
 // 1. getGamesOfType (filters between season/playoff games)
-// 2 getGamesBySeasonNumber filters games by season number
+// 2. getGamesBySeasonNumber filters games by season number
+// 3. getSingleGame
+// 4. query for games from specific team
+// 5. query for segment of games for ticker or scoreboard
 
 //  1. gets either season or playoff games
 async function getGamesOfType(gamesArray, gameType) {
@@ -21,16 +24,79 @@ async function getGamesOfType(gamesArray, gameType) {
 }
 
 // 2. filters games by season number
-W_GamesSchema.statics.getGamesBySeasonNumber = async function (
-  seasonNumber,
-  gameType
-) {
+W_GamesSchema.statics.getGamesBySeasonNumber = async function (seasonNumber) {
   // get a list of games by season number
   const gamesList = await this.find({
     "otherGameStats.seasonNumber": seasonNumber,
   });
-  const filteredGamesList = await getGamesOfType(gamesList, gameType);
-  return filteredGamesList;
+  return gamesList;
+};
+
+// 3. query for a single game
+W_GamesSchema.statics.getSingleGame = async function (gameId) {
+  const game = await this.findOne({ _id: gameId });
+  return game;
+};
+
+// 4. query for a teams games by game type
+W_GamesSchema.statics.getTeamsGames = async function (
+  seasonNumber,
+  gameType,
+  teamAcronym
+) {
+  const getSeasonGames = await this.find({
+    "otherGameStats.seasonNumber": seasonNumber,
+  });
+  const filteredGamesList = await getGamesOfType(getSeasonGames, gameType);
+  const teamsGames = filteredGamesList.filter(
+    (game) =>
+      game.otherGameStats.homeTeam == teamAcronym ||
+      game.otherGameStats.awayTeam == teamAcronym
+  );
+  return teamsGames;
+};
+
+// 5. query for segment of games for ticker or scoreboard
+W_GamesSchema.statics.getSelectedGames = async function (
+  seasonNumber,
+  beginIndex,
+  howManyGamesToGet
+) {
+  const seasonGames = await this.find({
+    "otherGameStats.seasonNumber": seasonNumber,
+  });
+  const totalGamesSubmitted = seasonGames.length;
+  const endIndex = beginIndex + howManyGamesToGet;
+  const selectedGames = seasonGames.slice(beginIndex, endIndex);
+  return { selectedGames, totalGamesSubmitted };
+};
+
+W_GamesSchema.statics.getFieldData = async function (
+  seasonNumber,
+  paramtersList,
+  requestedDataObject
+) {
+  const gamesList = await this.find({
+    "otherGameStats.seasonNumber": seasonNumber,
+  });
+  //////////////////////////////////
+  // if recent results are required
+  //////////////////////////////////
+  if (paramtersList.includes("recent-results")) {
+    const howManyGamesPlayed = gamesList.length;
+
+    let recentlyPlayedGames = [];
+    if (howManyGamesPlayed < 8) {
+      gamesList.forEach((game) => recentlyPlayedGames.push(game));
+    } else {
+      recentlyPlayedGames = gamesList.slice(howManyGamesPlayed - 8);
+    }
+
+    requestedDataObject.recentlyPlayedGames = recentlyPlayedGames;
+    requestedDataObject.totalGamesSubmitted = howManyGamesPlayed;
+  }
+
+  return requestedDataObject;
 };
 
 // end statics
