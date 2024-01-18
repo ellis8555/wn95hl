723a675176d4ca3cda96fda96b3369a1527c7c4f
ellis <lsp1@hotmail.com>
2024-01-18 10:55:34 -0700

    create static getTeamsGameStats

--- schemas/games/w_games.js
+++ schemas/games/w_games.js
@@ -1,7 +1,7 @@
 import { Schema, model, models } from "mongoose";
 import GameResultSchema from "./game-result/gameResult";
-
-const leagueName = "W";
+import Home_Team_Stats from "../home-team-game-stats/homeTeamGameStats";
+import Away_Team_Stats from "../away-team-game-stats/awayTeamGameStats";
 
 const W_GamesSchema = new Schema({
   ...GameResultSchema.obj,
@@ -13,6 +13,7 @@ const W_GamesSchema = new Schema({
 // 3. getSingleGame
 // 4. query for games from specific team
 // 5. query for segment of games for ticker or scoreboard
+// 6. get game stats for either home or away team
 
 //  1. gets either season or playoff games
 async function getGamesOfType(gamesArray, gameType) {
@@ -87,16 +88,51 @@ W_GamesSchema.statics.getFieldData = async function (
 
     let recentlyPlayedGames = [];
     if (howManyGamesPlayed < 8) {
-      gamesList.forEach((game) => recentlyPlayedGames.push(game));
+      for (const game of gamesList) {
+        // get related document in for home teams game stats
+        const getHomeTeamsGameStatsDoc = await Home_Team_Stats.find({
+          _id: game.homeTeamGameStats,
+        });
+        // get goals scored by the home team
+        const homeGoals = getHomeTeamsGameStatsDoc[0].gameStats.HomeGOALS;
+        // get related document in for away teams game stats
+        const getAwayTeamsGameStatsDoc = await Away_Team_Stats.find({
+          _id: game.awayTeamGameStats,
+        });
+        // get goals scored by the away team
+        const awayGoals = getAwayTeamsGameStatsDoc[0].gameStats.AwayGOALS;
+        // have to convert the mongoose doc into a javascript object in order to add new values
+        const gameObj = game.toObject();
+        // add each teams goals to othergamestats to be used for score ticker or other score related components
+        gameObj.otherGameStats["homeGoals"] = homeGoals;
+        gameObj.otherGameStats["awayGoals"] = awayGoals;
+        recentlyPlayedGames.push(gameObj);
+      }
     } else {
       recentlyPlayedGames = gamesList.slice(howManyGamesPlayed - 8);
     }
-
     requestedDataObject.recentlyPlayedGames = recentlyPlayedGames;
     requestedDataObject.totalGamesSubmitted = howManyGamesPlayed;
   }
 
   return requestedDataObject;
+};
+
+// 6. get home team stats
+W_GamesSchema.statics.getTeamsGameStats = async function (gameId, homeOrAway) {
+  const gameObject = await this.findOne({ _id: gameId });
+  let gameStatsDoc;
+  if (homeOrAway === "home") {
+    gameStatsDoc = await Home_Team_Stats.find({
+      _id: gameObject.homeTeamGameStats,
+    });
+  }
+  if (homeOrAway === "away") {
+    gameStatsDoc = await Away_Team_Stats.find({
+      _id: gameObject.awayTeamGameStats,
+    });
+  }
+  return gameStatsDoc;
 };
 
 // end statics
