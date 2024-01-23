c9812580b2839d6891ac01d1114a3e5e800ae7a3
ellis <lsp1@hotmail.com>
2024-01-23 11:32:59 -0700

    remove alert from homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Alert from "@/components/server/Alerts/Alert";
 import LeagueLogo from "@/components/server/Logos/LeagueLogo";
 
 export const metadata = {
@@ -14,8 +13,6 @@ export default async function Page() {
         <h1 className="text-4xl lg:text-6xl text-center pt-4">
           NHL95 in development
         </h1>
-        <Alert>Database maintenance near complete</Alert>
-        <Alert>Recent scores and team results page very limited</Alert>
         {/* beginning of league cards */}
         <div className="flex flex-col sm:flex-row justify-center mt-3 gap-3 md:gap-6 md:mt-6">
           <Link href="/league/w">
