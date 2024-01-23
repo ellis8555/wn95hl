e59355f1b7feb7db68a130ac350865e0ba87ed14
ellis <lsp1@hotmail.com>
2024-01-23 11:46:19 -0700

    alert message home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Alert from "@/components/server/Alerts/Alert";
 import LeagueLogo from "@/components/server/Logos/LeagueLogo";
 
 export const metadata = {
@@ -13,6 +14,13 @@ export default async function Page() {
         <h1 className="text-4xl lg:text-6xl text-center pt-4">
           NHL95 in development
         </h1>
+        <Alert>
+          On Wednesday Jan 24th attempt at re populating the entire database via
+          admins full season file
+        </Alert>
+        <Alert>
+          Success in this will lead to adding other leagues and past seasons!
+        </Alert>
         {/* beginning of league cards */}
         <div className="flex flex-col sm:flex-row justify-center mt-3 gap-3 md:gap-6 md:mt-6">
           <Link href="/league/w">
