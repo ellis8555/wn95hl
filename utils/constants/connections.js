2533de54f83240d3be0bcf7d1cd27b6b29ca3678
ellis <lsp1@hotmail.com>
2024-01-24 13:11:46 -0700

    connect to dev db

--- utils/constants/connections.js
+++ utils/constants/connections.js
@@ -1,6 +1,6 @@
 // export const DB_CONNECT = process.env.HOME_URI;
-export const DB_CONNECT = process.env.MONGODB_URI;
-// export const DB_CONNECT = process.env.MONGODB_DEV_URI;
+// export const DB_CONNECT = process.env.MONGODB_URI;
+export const DB_CONNECT = process.env.MONGODB_DEV_URI;
 
 // export const DOMAIN = "http://localhost:3000";
 export const DOMAIN = "https://nhl95.vercel.app";
