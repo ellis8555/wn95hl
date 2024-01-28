5655dd53179be41de2f2cb6916dca4a024529fca
ellis <lsp1@hotmail.com>
2024-01-28 16:08:56 -0700

    add q league card homepage

--- app/page.jsx
+++ app/page.jsx
@@ -29,6 +29,11 @@ export default async function Page() {
             seasonNumber="2"
             correspondingSeasonNumber="8"
           />
+          <LeagueCard
+            leagueName="q"
+            seasonNumber="88"
+            correspondingSeasonNumber="88"
+          />
         </div>
         {/* end of league cards */}
       </div>
