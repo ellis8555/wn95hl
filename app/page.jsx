08508309b09fe559165c4add2e47f7ed246a4f26
ellis <lsp1@hotmail.com>
2024-01-27 23:14:50 -0700

    create LeagueCard component

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
-import Link from "next/link";
 import Alert from "@/components/server/Alerts/Alert";
-import LeagueLogo from "@/components/server/Logos/LeagueLogo";
+import LeagueCard from "@/components/server/LeagueCard";
 
 export const metadata = {
   title: "NHL95",
@@ -14,18 +13,22 @@ export default async function Page() {
         <h1 className="text-4xl lg:text-6xl text-center pt-4">
           NHL95 in development
         </h1>
-        <Alert>Season 9 is complete!</Alert>
         <Alert>
-          Will rewrite the standings algorithm to properly reflect tie breakers
+          Tie in standings needs to be updated to reflect the leagues tie
+          breaker rules
         </Alert>
         {/* beginning of league cards */}
         <div className="flex flex-col sm:flex-row justify-center mt-3 gap-3 md:gap-6 md:mt-6">
-          <Link href="/league/w">
-            <div className="flex flex-col gap-6 border rounded-md p-8 shadow-2xl text-center">
-              <LeagueLogo name="w" width="100" height="100" />
-              <div>W standings</div>
-            </div>
-          </Link>
+          <LeagueCard
+            leagueName="w"
+            seasonNumber="3"
+            correspondingSeasonNumber="9"
+          />
+          <LeagueCard
+            leagueName="w"
+            seasonNumber="2"
+            correspondingSeasonNumber="8"
+          />
         </div>
         {/* end of league cards */}
       </div>
---
+++ components/server/LeagueCard.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import LeagueLogo from "./Logos/LeagueLogo";
+
+function LeagueCard({ leagueName, seasonNumber, correspondingSeasonNumber }) {
+  return (
+    <Link href={`/league/${leagueName}//${seasonNumber}`}>
+      <div className="flex flex-col gap-6 border rounded-md p-8 shadow-2xl text-center">
+        <LeagueLogo name={leagueName} width="100" height="100" />
+        <div>{`${leagueName.toUpperCase()}${correspondingSeasonNumber} standings`}</div>
+      </div>
+    </Link>
+  );
+}
+
+export default LeagueCard;
