8a9e1a6fac4b0de7b0c9acf8dc746cdfb29d2fe3
ellis <lsp1@hotmail.com>
2024-02-01 09:27:58 -0700

    add league switch to recent-results/league page

--- app/recent-scores/[league]/page.jsx
+++ app/recent-scores/[league]/page.jsx
@@ -1,30 +1,41 @@
 import GameResultsContainer from "@/components/client/Boxscore/GameResultsContainer";
 import { getRecentGameResults } from "@/app/(helpers)/get-recent-game-results";
 
-import { MOST_RECENT_SEASON } from "@/utils/constants/constants";
+import {
+  MOST_RECENT_SEASON,
+  MOST_RECENT_Q_SEASON,
+} from "@/utils/constants/constants";
 
 export const revalidate = 0;
 
 async function page({ params }) {
   const league = params.league;
+  // set season number dependant on which league
+  let latestSeason;
+  switch (league) {
+    case "w":
+      latestSeason = MOST_RECENT_SEASON;
+      break;
+    case "q":
+      latestSeason = MOST_RECENT_Q_SEASON;
+      break;
+  }
   // fetch most recent games
-  const gameData = JSON.parse(
-    await getRecentGameResults(league, MOST_RECENT_SEASON)
-  );
+  const gameData = JSON.parse(await getRecentGameResults(league, latestSeason));
 
   const { recentlyPlayedGames, totalGamesSubmitted } = gameData;
   const currentIndex = totalGamesSubmitted - 8;
   return (
     <div>
       <h1 className="text-xl text-center text-orange-400 my-3 md:text-3xl md:my-6">
-        Recent Scores
+        {`Recent Scores ${league.toUpperCase()}`}
       </h1>
       <GameResultsContainer
         recentGameResults={recentlyPlayedGames}
         totalGamesSubmitted={totalGamesSubmitted}
         currentIndex={currentIndex}
         leagueName={league}
-        seasonNumber={MOST_RECENT_SEASON}
+        seasonNumber={latestSeason}
       />
     </div>
   );
