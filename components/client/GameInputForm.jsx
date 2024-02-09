24ab588131875b9466548d3c7673d10314ab629b
ellis <lsp1@hotmail.com>
2024-02-09 11:01:43 -0700

    remove quotes from seasonNumber ref initialization

--- components/client/GameInputForm.jsx
+++ components/client/GameInputForm.jsx
@@ -29,7 +29,7 @@ function GameInputForm() {
   const fileInputRef = useRef(null);
   const gameTypeRef = useRef("");
   const leagueName = useRef("")
-  const seasonNumber = useRef("")
+  const seasonNumber = useRef()
 
   useEffect(() => {
     fetchGameData();
