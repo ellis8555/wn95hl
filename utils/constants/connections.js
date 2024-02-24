5f1302173d228f2faa892005268f7028e45c6a47
ellis <lsp1@hotmail.com>
2024-02-24 16:51:28 -0700

    connect to new official db darkside

--- utils/constants/connections.js
+++ utils/constants/connections.js
@@ -1,6 +1,7 @@
 // export const DB_CONNECT = process.env.HOME_URI;
-export const DB_CONNECT = process.env.MONGODB_URI;
+// export const DB_CONNECT = process.env.MONGODB_URI;
 // export const DB_CONNECT = process.env.MONGODB_DEV_URI;
+export const DB_CONNECT = process.env.MONGODB_DARKSIDE_URI;
 
 // export const DOMAIN = "http://localhost:3000";
 export const DOMAIN = "https://nhl95.vercel.app";
