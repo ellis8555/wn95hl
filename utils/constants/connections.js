d440ac381f1dd3c1e125f89b4b5c22854e0e1f0c
ellis <lsp1@hotmail.com>
2024-03-10 11:06:18 -0600

    add booleans/switch for single source dev mode

--- utils/constants/connections.js
+++ utils/constants/connections.js
@@ -1,12 +1,39 @@
-// export const DB_CONNECT = process.env.HOME_URI;
-// export const DB_CONNECT = process.env.MONGODB_URI;
-// export const DB_CONNECT = process.env.MONGODB_DEV_URI;
-export const DB_CONNECT = process.env.MONGODB_DARKSIDE_URI;
+// const CONNECTION_MODE = "DARK95_LIVE"
+const CONNECTION_MODE = "HOME_DEV"
+// const CONNECTION_MODE = "MONGO_ATLAS_95"
+// const CONNECTION_MODE = "MONGO_ATLAS_DEV"
 
-// export const DOMAIN = "http://localhost:3000";
-export const DOMAIN = "https://nhl95.vercel.app";
-// export const DOMAIN = "https://tiny-lokum-ab0acc.netlify.app";
+// true will save the game file to the selected database
+const WRITE_TO_DB = true;
 
-// export const ORIGIN = process.env.ALLOW_HOME_ORIGIN;
-export const ORIGIN = process.env.ALLOW_VERCEL_ORIGIN;
-// export const ORIGIN = process.env.ALLOW_NETLIFY_ORIGIN
+// true will allow duplicates to pass through game state duplicate check
+const ALLOW_DUPLICATES = false;
+
+let DB_CONNECT;
+let DOMAIN;
+let ORIGIN
+
+switch(CONNECTION_MODE){
+    case "HOME_DEV":
+        DB_CONNECT = process.env.HOME_URI;
+        DOMAIN = "http://localhost:3000";
+        ORIGIN = process.env.ALLOW_HOME_ORIGIN;
+    break;
+    case "MONGO_ATLAS_95":
+        DB_CONNECT = process.env.MONGODB_URI;
+        DOMAIN = "http://localhost:3000";
+        ORIGIN = process.env.ALLOW_HOME_ORIGIN;
+        break;
+    case "MONGO_ATLAS_DEV":
+        DB_CONNECT = process.env.MONGODB_DEV_URI;
+        DOMAIN = "http://localhost:3000";
+        ORIGIN = process.env.ALLOW_HOME_ORIGIN;
+        break;
+    case "DARK95_LIVE":
+        DB_CONNECT = process.env.MONGODB_DARKSIDE_URI;
+        DOMAIN = "https://nhl95.vercel.app";
+        ORIGIN = process.env.ALLOW_VERCEL_ORIGIN;
+        break;
+}
+
+export {DB_CONNECT, DOMAIN, ORIGIN, WRITE_TO_DB, ALLOW_DUPLICATES}
