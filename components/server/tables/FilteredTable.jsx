77070cdaac0eaa4f691fc198d16adb2614a58cf3
ellis <lsp1@hotmail.com>
2024-03-12 12:14:48 -0600

    league/filtered tables take props to allow sorting

--- components/server/tables/FilteredTable.jsx
+++ components/server/tables/FilteredTable.jsx
@@ -9,11 +9,13 @@ function FilteredTable({
   standings,
   divisions,
   isTableFiltered,
+  setAreStandingsSorted,
+  setSortedStandings
 }) {
   return (
     <div className="overflow-auto">
     <table className="my-4 w-full md:w-3/4 md:mx-auto table-auto">
-<TableHeaders leagueName={leagueName} seasonNumber={seasonNumber} standings={standings}/>
+<TableHeaders leagueName={leagueName} seasonNumber={seasonNumber} standings={standings} setAreStandingsSorted={setAreStandingsSorted} setSortedStandings={setSortedStandings}/>
       <tbody>
         {standings.length > 0 ? (
           standings
--- components/server/tables/LeagueTable.jsx
+++ components/server/tables/LeagueTable.jsx
@@ -2,11 +2,11 @@ import Teamresults from "../standings/Teamresults";
 import TableHeaders from "@/components/client/Tables/Table-headers";
 import { LEAGUE_TABLE_CATEGORIES } from "@/utils/constants/constants";
 
-function LeagueTable({ leagueName, seasonNumber, standings, isTableFiltered }) {
+function LeagueTable({ leagueName, seasonNumber, standings, isTableFiltered, setAreStandingsSorted, setSortedStandings }) {
   return (
     <div className="overflow-auto">
     <table className="my-4 w-full md:w-3/4 md:mx-auto table-auto">
-    <TableHeaders leagueName={leagueName} seasonNumber={seasonNumber} standings={standings}/>
+    <TableHeaders leagueName={leagueName} seasonNumber={seasonNumber} standings={standings} setAreStandingsSorted={setAreStandingsSorted} setSortedStandings={setSortedStandings}/>
       <tbody>
         {standings.length > 0 ? (
           standings.map((team, index) => (
