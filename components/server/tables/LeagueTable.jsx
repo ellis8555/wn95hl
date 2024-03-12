100942e062999bdaf902d775a0acb3851791856a
ellis <lsp1@hotmail.com>
2024-03-12 00:49:45 -0600

    and TableHeaders component to standing tables

--- components/server/tables/LeagueTable.jsx
+++ components/server/tables/LeagueTable.jsx
@@ -1,20 +1,12 @@
 import Teamresults from "../standings/Teamresults";
+import TableHeaders from "@/components/client/Tables/Table-headers";
 import { LEAGUE_TABLE_CATEGORIES } from "@/utils/constants/constants";
 
 function LeagueTable({ leagueName, seasonNumber, standings, isTableFiltered }) {
   return (
     <div className="overflow-auto">
     <table className="my-4 w-full md:w-3/4 md:mx-auto table-auto">
-      <thead>
-        <tr className="text-slate-300 bg-slate-800">
-          <th className="text-xl bg-slate-800 z-10 sticky left-0">Team</th>
-          {LEAGUE_TABLE_CATEGORIES.map((header, index) => (
-            <th className="p-4 sm:text-xl" key={index}>
-              {header}
-            </th>
-          ))}
-        </tr>
-      </thead>
+    <TableHeaders leagueName={leagueName} seasonNumber={seasonNumber} standings={standings}/>
       <tbody>
         {standings.length > 0 ? (
           standings.map((team, index) => (
