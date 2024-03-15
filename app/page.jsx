12d691403609066d9018d4aa5a652bdd199c9aaf
ellis <lsp1@hotmail.com>
2024-03-15 10:36:26 -0600

    home page alert link to projects news

--- app/page.jsx
+++ app/page.jsx
@@ -14,9 +14,8 @@ export default async function Page() {
         <h1 className="text-4xl lg:text-6xl text-center pt-4">
           Darkside Stats
         </h1>
-        <Alert backgroundColor="bg-green-500">Tables can be sorted. Click on column header and it will sort by that header.</Alert>
         <Alert>
-        <Link className="underline" href="/news">UPDATED: game state uploads</Link>
+        <Link className="underline" href="/news">Notes on this project</Link>
         </Alert>
         {/* beginning of league cards */}
         <div className="flex flex-col sm:flex-row justify-center mt-3 gap-3 md:gap-6 md:mt-6">
