996a2c37b5745161f4d7f28b7fafca36d8f8cb33
ellis <lsp1@hotmail.com>
2024-03-17 19:48:35 -0600

    points percentage add 1 to .000 for perfect record

--- utils/api/table-methods/team-standings/update-points-percentage.js
+++ utils/api/table-methods/team-standings/update-points-percentage.js
@@ -1,9 +1,18 @@
 function updatePointsPercentage(arrayOfTeamObjects, teamsStandingsIndex){
+    // get total games played by team
     const gamesPlayed = arrayOfTeamObjects[teamsStandingsIndex].GP;
+    // get teams total points
     const totalPoints = arrayOfTeamObjects[teamsStandingsIndex].Pts
-
+    // calculate teams point percentage
     const pointsPercentage = totalPoints/(gamesPlayed*2)
-    const pointsTrimmed = "."+pointsPercentage.toFixed(3).split('.')[1];
+    // check if team has all wins in order to display 1.000
+    let pointsTrimmed
+    if((gamesPlayed > 0) && (gamesPlayed*2 ) === totalPoints){
+        pointsTrimmed = "1."+pointsPercentage.toFixed(3).split('.')[1];
+    } else {
+        // team does not have all wins therefore format is .ddd
+        pointsTrimmed = "."+pointsPercentage.toFixed(3).split('.')[1];
+    }
     arrayOfTeamObjects[teamsStandingsIndex]["Pts%"] = pointsTrimmed.toString()
     return arrayOfTeamObjects[teamsStandingsIndex]
 }
