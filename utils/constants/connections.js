5f99a53d3435b94c55cf5a08d82244f2641a30d2
ellis <lsp1@hotmail.com>
2024-03-20 09:54:09 -0600

    add darkURI to homeDev in connection consts

--- utils/constants/connections.js
+++ utils/constants/connections.js
@@ -9,6 +9,7 @@ const CONNECTION_MODE = "DARK95_LIVE"
 /////////////////////////////
 
 // const CONNECTION_MODE = "HOME_DEV"
+// const CONNECTION_MODE = "HOME_DARK_DEV"
 // const CONNECTION_MODE = "MONGO_ATLAS_95"
 // const CONNECTION_MODE = "MONGO_ATLAS_DEV"
 
@@ -25,6 +26,11 @@ let ORIGIN
 switch(CONNECTION_MODE){
     case "HOME_DEV":
         DB_CONNECT = process.env.HOME_URI;
+        DOMAIN = "http://localhost:3000";
+        ORIGIN = process.env.ALLOW_HOME_ORIGIN;
+    break;
+    case "HOME_DARK_DEV":
+        DB_CONNECT = process.env.MONGODB_DARKSIDE_URI;
         DOMAIN = "http://localhost:3000";
         ORIGIN = process.env.ALLOW_HOME_ORIGIN;
     break;
