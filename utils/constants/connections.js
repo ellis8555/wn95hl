0ca642dd7727f2793a360b64ef78919c377c7dd5
ellis <lsp1@hotmail.com>
2024-03-20 09:54:09 -0600

    add darkURI to homeDev in connection consts

--- utils/constants/connections.js
+++ utils/constants/connections.js
@@ -2,18 +2,19 @@
 // THIS IS THE LIVE SETTING
 ///////////////////////////
 
-const CONNECTION_MODE = "DARK95_LIVE"
+// const CONNECTION_MODE = "DARK95_LIVE"
 
 /////////////////////////////
 // THESE ARE TESTING SETTINGS
 /////////////////////////////
 
 // const CONNECTION_MODE = "HOME_DEV"
+const CONNECTION_MODE = "HOME_DARK_DEV"
 // const CONNECTION_MODE = "MONGO_ATLAS_95"
 // const CONNECTION_MODE = "MONGO_ATLAS_DEV"
 
 // true will save the game file to the selected database
-const WRITE_TO_DB = true;
+const WRITE_TO_DB = false;
 
 // true will allow duplicates to pass through game state duplicate check
 const ALLOW_DUPLICATES = false;
@@ -25,6 +26,11 @@ let ORIGIN
 switch(CONNECTION_MODE){
     case "HOME_DEV":
         DB_CONNECT = process.env.HOME_URI;
+        DOMAIN = "http://localhost:3000";
+        ORIGIN = process.env.ALLOW_HOME_ORIGIN;
+    break;
+    case "HOME_DARK_DEV":
+        DB_CONNECT = process.env.MONGODB_DARKSIDE_URI;
         DOMAIN = "http://localhost:3000";
         ORIGIN = process.env.ALLOW_HOME_ORIGIN;
     break;
