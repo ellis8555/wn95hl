6e49d50079e6db4f15c3df44ce521eb6bdf5ec37
ellis <lsp1@hotmail.com>
2024-03-29 19:54:16 -0600

    dashboard add reverse-submit route

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -7,8 +7,11 @@ function Page() {
       <div className="text-center">
         <ul className="inline-block ps-2 sm:p-0 text-left list-none">
           <li>You can sign out via the green arrow on the navigation bar</li>
-          <li className="text-green-400">
+          {/* <li className="text-green-400">
             <Link href="/recent-scores">Edit a game</Link>
+          </li> */}
+          <li className="text-green-400">
+            <Link href="/reverse-submit">Reverse teams submit</Link>
           </li>
         </ul>
       </div>
