a53395627c1cfd0f6e8a0cf44a93e81bafef6939
ellis <lsp1@hotmail.com>
2024-03-30 20:01:58 -0600

    redirect user if user auth and on login page

--- middleware.js
+++ middleware.js
@@ -1,15 +1,26 @@
 import { NextResponse } from "next/server";
 import { AUTH_COOKIE } from "./utils/constants/constants";
 
-export async function middleware(req) {
+export async function middleware(req, res) {
   const isAuthenticated = req.cookies.has(AUTH_COOKIE);
-  if (!isAuthenticated) {
+  const url = new URL(req.url)
+  const path = url.pathname
+
+  // list of routes that are protected
+  const protectedPaths = ["/dashboard", "/reverse-submit", "/edit-boxscore"];
+  // returns unauthorized user to login page if not authorized and on one of the above protected paths
+  if (protectedPaths.includes(path) && !isAuthenticated) {
     return NextResponse.redirect(new URL("/login", req.url));
+  }
+  
+  // prevents users already logged in being able to navigate to the login page
+  if(path == "/login" && isAuthenticated){
+    return NextResponse.redirect(new URL("/logout", req.url));
   }
 
   NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard", "/reverse-submit", "/edit-boxscore"],
+  matcher: ["/login", "/dashboard", "/reverse-submit", "/edit-boxscore"],
 };
