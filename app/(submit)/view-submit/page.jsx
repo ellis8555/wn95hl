f9b69532a25c305ae302b685c116b18f28863cae
ellis <lsp1@hotmail.com>
2024-04-01 01:01:07 -0600

    create view-submit page

---
+++ app/(submit)/view-submit/page.jsx
@@ -0,0 +1,11 @@
+import ViewGameStateSubmitForm from "@/components/client/GameInput/ViewGameState";
+
+function page() {
+  return (
+    <div className="h-96 mt-4 text-slate-300">
+      <ViewGameStateSubmitForm />
+    </div>
+  );
+}
+
+export default page;
