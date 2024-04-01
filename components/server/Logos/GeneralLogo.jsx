43f5d105498e87f264ddb530225b8456d6a445af
ellis <lsp1@hotmail.com>
2024-03-31 21:39:58 -0600

    set priority on general logo

--- components/server/Logos/GeneralLogo.jsx
+++ components/server/Logos/GeneralLogo.jsx
@@ -9,6 +9,7 @@ function GeneralLogo({ name, width, height }) {
       style={{ width: "auto", height: "auto" }}
       alt={name}
       quality={100}
+      priority={name == "NHL95-sprites-banner" ? true:false}
     />
   );
 }
