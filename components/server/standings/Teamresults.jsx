8acf11e997d876efbc920a454520fb446afee337
ellis <lsp1@hotmail.com>
2024-04-06 23:03:23 -0600

    move team beside GP if index zero for border style indicating playoff

--- components/server/standings/Teamresults.jsx
+++ components/server/standings/Teamresults.jsx
@@ -10,59 +10,66 @@ function Teamresults({
   seasonNumber,
 }) {
 
-function tablePositionBorder(leagueName, lineNumber){
-  switch(leagueName){
-    case "q":
-      if(lineNumber <=3){
+  function tablePositionBorder(leagueName, lineNumber) {
+    switch(leagueName){
+      case "q":
+        if(lineNumber <=3){
+          return "border-solid border-green-700 border-l-4"
+        }
+        if(lineNumber >=16){
+          return "border-solid border-red-900 border-l-4"
+        }
+        return "border-solid border-blue-700 border-l-4"
+      case "w":
+        if(lineNumber >=16){
+          return "border-solid border-red-900 border-l-4"
+        }
         return "border-solid border-green-700 border-l-4"
-      }
-      if(lineNumber >=16){
-        return "border-solid border-red-900 border-l-4"
-      }
-      return "border-solid border-blue-700 border-l-4"
-    case "w":
-      if(lineNumber >=16){
-        return "border-solid border-red-900 border-l-4"
-      }
-      return "border-solid border-green-700 border-l-4"
-    default:
-      return ""
+      default:
+        return ""
+    }
   }
-}
 
   return (
-      <tr
-      className={`${bgColor} text-sm h-10`}
-    >
-      <td className={`flex justify-center sticky left-0 ${bgColor}`}>
-        <div className={`flex items-center h-10 my-1 px-2 ${tablePositionBorder(leagueName, lineNumber)}`}>
-          <TeamLogo
-            name={team.teamLogo}
-            width={25}
-            height={15}
-            leagueName={leagueName}
-            seasonNumber={seasonNumber}
-          />
-        </div>
-      </td>
+    <tr className={`${bgColor} text-sm h-10`}>
       {categories.map((category, index) => {
-        // adjust color for goals diff column to set dynamic colors
-        let dynamicTextColor = {bgColor};
-        if(category === "Diff"){
-          if(team[category] > 0){
-            dynamicTextColor = "text-green-700"
+        let dynamicTextColor = bgColor;
+        if (category === "Diff") {
+          if (team[category] > 0) {
+            dynamicTextColor = "text-green-700";
           }
-          if(team[category] < 0){
-            dynamicTextColor = "text-red-900"
+          if (team[category] < 0) {
+            dynamicTextColor = "text-red-900";
           }
         }
-      return (
-        <td key={index} category={category} className={`text-center ${dynamicTextColor} whitespace-nowrap`}>
-          {(category === "Diff" && team[category] > 0 ) ? "+"+team[category]:team[category]}
-        </td>
-      )})}
+        if (index === 0) {
+          return [
+            <td key={`${index}-teamLogo`} className={`flex justify-center sticky left-0 ${bgColor}`}>
+              <div className={`flex items-center h-10 my-1 px-2 ${tablePositionBorder(leagueName, lineNumber)}`}>
+                <TeamLogo
+                  name={team.teamLogo}
+                  width={25}
+                  height={15}
+                  leagueName={leagueName}
+                  seasonNumber={seasonNumber}
+                />
+              </div>
+            </td>,
+            <td key={index} category={category} className={`text-center ${dynamicTextColor} whitespace-nowrap`}>
+              {(category === "Diff" && team[category] > 0) ? "+" + team[category] : team[category]}
+            </td>
+          ];
+        } else {
+          return (
+            <td key={index} category={category} className={`text-center ${dynamicTextColor} whitespace-nowrap`}>
+              {(category === "Diff" && team[category] > 0) ? "+" + team[category] : team[category]}
+            </td>
+          );
+        }
+      })}
     </tr>
-      )
+  );
 }
 
 export default Teamresults;
+
