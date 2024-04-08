7da5f9925b34329810d583b02e82dd97e9bcf72a
ellis <lsp1@hotmail.com>
2024-04-08 11:21:01 -0600

    pure league upload message home screen

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ export default async function Page() {
         <h1 className="text-4xl lg:text-6xl text-center pt-4">
           Darkside Stats
         </h1>
-        <Alert>Will work on adding Pure league soon</Alert>
+        <Alert backgroundColor={"bg-green-600"}>Pure League game state upload tests needed. Live uploads should be enabled soon.</Alert>
         {/* beginning of league cards */}
         <div className="flex flex-col sm:flex-row justify-center mt-3 gap-3 md:gap-6 md:mt-6">
           <LeagueCard
