30bbd194d366166d8d20f4f2b38b0e2132b70297
ellis <lsp1@hotmail.com>
2024-04-14 10:17:53 -0600

    update pure league game uploads to properly remove empty players

--- components/client/GameInput/GameInputForm.jsx
+++ components/client/GameInput/GameInputForm.jsx
@@ -158,11 +158,14 @@ function GameInputForm() {
       //////////////////////////////
        else if(PURE_LEAGUE_STATE_PATTERN.test(fileName)){
         //FIXME: temp disable pure league uploads
-        setServerMessage("Pure league game state uploads coming soon. No data was uploaded")
+        setServerMessage("Pure league uploads currently disabled")
         setIsSubmitting(false)
         fileInputRef.current.value = null;
         return;
         //FIXME: end temp disable pure league uploads
+        setServerMessage("Game file being processed...")
+        setIsSubmitting(false)
+        fileInputRef.current.value = null;
          leagueName.current = "p";
          seasonNumber.current = MOST_RECENT_P_SEASON;
          const fetchedGameData = await readOgRomBinaryGameState(
@@ -171,34 +174,34 @@ function GameInputForm() {
            leagueName.current,
           );
         // trim extra player slots due to teams having different amount of players/goalies
-        //trim the goalies
-        if(fetchedGameData.data.homeTeamGoalieStats[2].Name == "-"){
-          fetchedGameData.data.homeTeamGoalieStats.splice(2);
-        }
-        if(fetchedGameData.data.awayTeamGoalieStats[2].Name == "-"){
+        // trim empty goalie slots as some teams have 2 goalies while others 3 goalies
+        if(fetchedGameData.data.awayTeamGoalieStats[2]['Name'] === "-"){
           fetchedGameData.data.awayTeamGoalieStats.splice(2);
         }
-        // trim the players
-        // ASW only has 18 players
-        if(fetchedGameData.data.homeTeamPlayerStats[18].Name == "-"){
-          fetchedGameData.data.homeTeamPlayerStats.splice(18);
+        if(fetchedGameData.data.homeTeamGoalieStats[2]['Name'] === "-"){
+          fetchedGameData.data.homeTeamGoalieStats.splice(2);
         }
-        if(fetchedGameData.data.awayTeamPlayerStats[18].Name == "-"){
+        // from here on optional chaining needs to occur due to some teams short of players won't have indexes higher than 18
+        // some teams only have 18 skaters
+        if(fetchedGameData.data.awayTeamPlayerStats[18]?.Name === "-"){
           fetchedGameData.data.awayTeamPlayerStats.splice(18);
         }
-        // ASE and TB only has 20 players
-        if(fetchedGameData.data.homeTeamPlayerStats[20].Name == "-"){
-          fetchedGameData.data.homeTeamPlayerStats.splice(20);
+        if(fetchedGameData.data.homeTeamPlayerStats[18]?.Name === "-"){
+          fetchedGameData.data.homeTeamPlayerStats.splice(18);
         }
-        if(fetchedGameData.data.awayTeamPlayerStats[20].Name == "-"){
+        // some teams only have 20 skaters
+        if(fetchedGameData.data.awayTeamPlayerStats[20]?.Name === "-"){
           fetchedGameData.data.awayTeamPlayerStats.splice(20);
         }
-        // several teams have only 21 players
-        if(fetchedGameData.data.homeTeamPlayerStats[21].Name == "-"){
-          fetchedGameData.data.homeTeamPlayerStats.splice(21);
+        if(fetchedGameData.data.homeTeamPlayerStats[20]?.Name === "-"){
+          fetchedGameData.data.homeTeamPlayerStats.splice(20);
         }
-        if(fetchedGameData.data.awayTeamPlayerStats[21].Name == "-"){
+        // some teams only have 21 skaters
+        if(fetchedGameData.data.awayTeamPlayerStats[21]?.Name === "-"){
           fetchedGameData.data.awayTeamPlayerStats.splice(21);
+        }
+        if(fetchedGameData.data.homeTeamPlayerStats[21]?.Name === "-"){
+          fetchedGameData.data.homeTeamPlayerStats.splice(21);
         }
         gameStatesData.push(fetchedGameData);
         setLeagueContext(leagueName.current);
