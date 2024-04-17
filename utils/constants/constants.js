0a63cd4bfcdda46f99a6f3f513b64c69b615e99c
ellis <lsp1@hotmail.com>
2024-04-17 08:21:49 -0600

    move Manager after Team htmx standings

--- utils/constants/constants.js
+++ utils/constants/constants.js
@@ -1,7 +1,7 @@
 export const LEAGUE_TABLE_CATEGORIES = ["Manager", "GP", "W", "L", "T", "OTL", "Pts","Pts%", "Gf", "Ga", "Diff", "Strk", "L10", "Gfa", "Gaa", "SO"];
 export const LEAGUE_HTMX_TABLE_CATEGORIES = [
-  "Manager",
   "Team",
+  "Manager",
   "GP",
   "W",
   "L",
