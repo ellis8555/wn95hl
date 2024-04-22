e8c01f2a29b1b182f007267cb99647029ed815c4
ellis <lsp1@hotmail.com>
2024-04-22 09:39:53 -0600

    home message playoff states can be viewed

--- app/page.jsx
+++ app/page.jsx
@@ -13,6 +13,7 @@ export default async function Page() {
         <h1 className="text-4xl lg:text-6xl text-center pt-4">
           Darkside Stats
         </h1>
+        <Alert>Playoff states can be viewed via 'View a state'. No database entry will occur</Alert>
         {/* beginning of league cards */}
         <div className="flex flex-col sm:flex-row justify-center mt-3 gap-3 md:gap-6 md:mt-6">
           <LeagueCard
