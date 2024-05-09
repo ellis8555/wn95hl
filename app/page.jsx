fe8b4aa5dc5833d661a6184b4cbea59cdee7ba98
ellis <lsp1@hotmail.com>
2024-05-09 11:13:16 -0600

    home message unimportant

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ export default async function Page() {
         <h1 className="text-4xl lg:text-6xl text-center pt-4">
           Darkside Stats
         </h1>
-        <Alert backgroundColor={"bg-green-600"}>I've decided to discontinue this project. I will finish out updating the W and Vintage until seasons end. Ultimately the overall goal was to enable each user to upload their own games states. This has been accomplished. Aside from updating this very site, admins could login and download a file that contains data for games to be added to the leagues sheets. I've been personally uploading each and every game states (Ceydan also uploaded some) as they were dropped into the discord ensuring all is working for several seasons. Whether it was me or everyone else uploading games makes no difference.</Alert>
+        <Alert backgroundColor={"bg-green-600"}>I've decided to discontinue this project. I will finish out updating the W and Vintage until seasons end. Ultimately the overall goal was to enable each user to upload their own games states. This has been accomplished in a manner that both satisfies any website built while also providing what is needed to update the sheets.</Alert>
         <Alert backgroundColor={"bg-green-600"}>I will add cards on this homepage for each season that is currently stored in the database as well.</Alert>
         <Alert backgroundColor={"bg-green-600"}>As a personal hobby I will also work on a playoff bracket page for Q89 as I have saved all the playoff states for that season.</Alert>
         {/* beginning of league cards */}
